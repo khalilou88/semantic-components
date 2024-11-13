b35de6edd6c84a22bcd91fceacdcbf9befa85909
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-11-13 23:01:01 +0100

    work in progress

---
+++ libs/ui/src/components/svg-icons/close-svg.component.ts
@@ -0,0 +1,30 @@
+import { CommonModule, NgClass } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'sc-close-svg',
+  standalone: true,
+  imports: [CommonModule, NgClass],
+  template: `
+    <svg
+      [ngClass]="class"
+      aria-hidden="true"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+        clip-rule="evenodd"
+      ></path>
+    </svg>
+  `,
+  styles: ``,
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CloseSvgComponent {
+  @Input()
+  class = '';
+}
