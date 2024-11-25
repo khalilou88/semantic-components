c14028b77c4c37a93706d012c8acc9cee355d63a
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-11-25 11:23:20 +0100

    work in progress

--- libs/nx-generators/src/generators/heroicons/generator.ts
+++ libs/nx-generators/src/generators/heroicons/generator.ts
@@ -4,14 +4,18 @@ import * as path from 'path';
 import { HeroiconsGeneratorSchema } from './schema';
 
 export async function heroiconsGenerator(tree: Tree, options: HeroiconsGeneratorSchema) {
-  const projectRoot = `libs/${options.name}`;
-  addProjectConfiguration(tree, options.name, {
-    root: projectRoot,
-    projectType: 'library',
-    sourceRoot: `${projectRoot}/src`,
-    targets: {},
-  });
-  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+  // const projectRoot = `libs/${options.name}`;
+
+  const iconsFolder = 'libs/heroicons/src/icons';
+
+  // addProjectConfiguration(tree, options.name, {
+  //   root: projectRoot,
+  //   projectType: 'library',
+  //   sourceRoot: `${projectRoot}/src`,
+  //   targets: {},
+  // });
+
+  generateFiles(tree, path.join(__dirname, 'files'), iconsFolder, options);
   await formatFiles(tree);
 }
 
