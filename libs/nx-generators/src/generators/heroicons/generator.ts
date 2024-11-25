9511cec325a613ac8cecf3a63cb391243bdcee9c
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-11-25 13:29:57 +0100

    work in progress

--- libs/nx-generators/src/generators/heroicons/generator.ts
+++ libs/nx-generators/src/generators/heroicons/generator.ts
@@ -8,29 +8,57 @@ import path = require('path');
 export async function heroiconsGenerator(tree: Tree, options: HeroiconsGeneratorSchema) {
   const iconsDestinationPath = 'libs/heroicons/src/icons';
 
+  //1
   const solid16IconsSourcePath =
     'libs/nx-generators/src/generators/heroicons/files/optimized/16/solid';
   const solid16IconsDestinationPath = path.join(iconsDestinationPath, '16', 'solid');
 
-  tree.children(solid16IconsSourcePath).forEach((fileName) => {
-    console.log(fileName);
+  generateIconsComponents(tree, solid16IconsSourcePath, solid16IconsDestinationPath);
 
-    const svgContent = tree.read(path.join(solid16IconsSourcePath, fileName), 'utf-8').toString();
+  //2
+  const solid20IconsSourcePath =
+    'libs/nx-generators/src/generators/heroicons/files/optimized/20/solid';
+  const solid20IconsDestinationPath = path.join(iconsDestinationPath, '20', 'solid');
+
+  generateIconsComponents(tree, solid20IconsSourcePath, solid20IconsDestinationPath);
+
+  //3
+  const outline24IconsSourcePath =
+    'libs/nx-generators/src/generators/heroicons/files/optimized/24/outline';
+  const outline24IconsDestinationPath = path.join(iconsDestinationPath, '24', 'outline');
+
+  generateIconsComponents(tree, outline24IconsSourcePath, outline24IconsDestinationPath);
+
+  //4
+  const solid24IconsSourcePath =
+    'libs/nx-generators/src/generators/heroicons/files/optimized/24/solid';
+  const solid24IconsDestinationPath = path.join(iconsDestinationPath, '24', 'solid');
+
+  generateIconsComponents(tree, solid24IconsSourcePath, solid24IconsDestinationPath);
+
+  await formatFiles(tree);
+}
+
+function generateIconsComponents(
+  tree: Tree,
+  solid16IconsSourcePath: string,
+  solid16IconsDestinationPath: string,
+) {
+  tree.children(solid16IconsSourcePath).forEach((fileName) => {
+    const svgContent = tree.read(path.join(solid16IconsSourcePath, fileName), 'utf-8');
     const svgClassName = `${names(fileName).className}Icon`;
     const svgFileName = `${names(fileName).fileName}-icon`;
     const svgSelector = `${names(fileName).fileName}-icon`;
 
-    const my_options = { svgContent, svgClassName, svgFileName, svgSelector };
+    const o = { svgContent, svgClassName, svgFileName, svgSelector };
 
     generateFiles(
       tree,
       path.join(__dirname, 'files', 'src', 'component'),
       solid16IconsDestinationPath,
-      my_options,
+      o,
     );
   });
-
-  await formatFiles(tree);
 }
 
 export default heroiconsGenerator;
