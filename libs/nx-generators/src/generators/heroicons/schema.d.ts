19298d5b56f26854cdaa8df02f84cd440a249b7e
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-11-25 13:33:46 +0100

    work in progress

--- libs/nx-generators/src/generators/heroicons/schema.d.ts
+++ libs/nx-generators/src/generators/heroicons/schema.d.ts
@@ -1,3 +1,2 @@
-export interface HeroiconsGeneratorSchema {
-  name: string;
-}
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type, @typescript-eslint/no-empty-interface
+export interface HeroiconsGeneratorSchema {}
