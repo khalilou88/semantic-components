8baf6f90b3f8bc773a75ef8599539b2474941756
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-11-25 13:50:36 +0100

    work in progress

--- libs/nx-generators/src/generators/heroicons/generator.ts
+++ libs/nx-generators/src/generators/heroicons/generator.ts
@@ -6,33 +6,57 @@ import path = require('path');
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function heroiconsGenerator(tree: Tree, options: HeroiconsGeneratorSchema) {
-  const iconsDestinationPath = 'libs/heroicons/src/icons';
+  const iconsDestinationPath = 'libs/heroicons';
 
   //1
   const solid16IconsSourcePath =
     'libs/nx-generators/src/generators/heroicons/files/optimized/16/solid';
-  const solid16IconsDestinationPath = path.join(iconsDestinationPath, 'solid', '16');
+  const solid16IconsDestinationPath = path.join(
+    iconsDestinationPath,
+    'solid',
+    '16',
+    'src',
+    'icons',
+  );
 
   generateIconsComponents(tree, solid16IconsSourcePath, solid16IconsDestinationPath);
 
   //2
   const solid20IconsSourcePath =
     'libs/nx-generators/src/generators/heroicons/files/optimized/20/solid';
-  const solid20IconsDestinationPath = path.join(iconsDestinationPath, 'solid', '20');
+  const solid20IconsDestinationPath = path.join(
+    iconsDestinationPath,
+    'solid',
+    '20',
+    'src',
+    'icons',
+  );
 
   generateIconsComponents(tree, solid20IconsSourcePath, solid20IconsDestinationPath);
 
   //3
   const outline24IconsSourcePath =
     'libs/nx-generators/src/generators/heroicons/files/optimized/24/outline';
-  const outline24IconsDestinationPath = path.join(iconsDestinationPath, 'outline', '24');
+  const outline24IconsDestinationPath = path.join(
+    iconsDestinationPath,
+    'outline',
+    '24',
+    'src',
+    'icons',
+  );
 
   generateIconsComponents(tree, outline24IconsSourcePath, outline24IconsDestinationPath);
 
   //4
   const solid24IconsSourcePath =
     'libs/nx-generators/src/generators/heroicons/files/optimized/24/solid';
-  const solid24IconsDestinationPath = path.join(iconsDestinationPath, 'solid', '24');
+  const solid24IconsDestinationPath = path.join(
+    iconsDestinationPath,
+    'solid',
+    '24',
+    'src',
+    'icons',
+  );
 
   generateIconsComponents(tree, solid24IconsSourcePath, solid24IconsDestinationPath);
 
