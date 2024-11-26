4b2c19b765a3e63952ff40c476aa1205402795f9
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-11-26 11:29:52 +0100

    work in progress

--- libs/heroicons/24/outline/src/icons/svg-cpu-chip-icon.ts
+++ libs/heroicons/24/outline/src/icons/svg-cpu-chip-icon.ts
@@ -7,6 +7,7 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation, input } from '@a
   imports: [NgClass],
   template: `
     <svg
+      [ngClass]="class()"
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
