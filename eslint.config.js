1cac0bf553eac46319ee30fb34805269eb218718
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-05 13:30:49 +0100

    build: work in progress

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,13 @@
 const nx = require('@nx/eslint-plugin');
 const tailwind = require('eslint-plugin-tailwindcss');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 module.exports = [
   ...nx.configs['flat/base'],
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
   ...tailwind.configs['flat/recommended'],
+  eslintPluginPrettierRecommended,
   {
     ignores: ['**/dist'],
   },
