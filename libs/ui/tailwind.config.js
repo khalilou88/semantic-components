5da59776ec4bb6bb1d6382c3f95eb89098579c6b
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-05 15:05:44 +0100

    build: work in progress

--- libs/ui/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-const { join } = require('path');
-const tailwindWorkspacePreset = require('../../tailwind-workspace-preset');
-
-module.exports = {
-  presets: [tailwindWorkspacePreset],
-  content: [join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}')],
-};
