2744f95da47cb6a1f09dd20935863da59bd4addf
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-05 14:58:55 +0100

    build: work in progress

--- apps/showcase/src/app/pages/editor-page.ts
+++ apps/showcase/src/app/pages/editor-page.ts
@@ -1,15 +1,33 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 import { ScEditor } from '@semantic-components/ui';
 
 @Component({
   selector: 'app-editor-page',
-  imports: [ScEditor],
+  imports: [ScEditor, ReactiveFormsModule],
   template: `
-    <sc-editor />
+    <form [formGroup]="editorForm">
+      <sc-editor formControlName="content" />
+    </form>
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class EditorPage {}
+export default class EditorPage {
+  editorForm = new FormGroup({
+    content: new FormControl(`  
+      <h1>Garlic bread with cheese: What the science tells us</h1>
+  <p>
+    For years parents have espoused the health benefits of eating garlic bread with cheese to their
+    children, with the food earning such an iconic status in our culture that kids will often dress
+    up as warm, cheesy loaf for Halloween.
+  </p>
+  <p>
+    But a recent study shows that the celebrated appetizer may be linked to a series of rabies cases
+    springing up around the country.
+  </p>
+  `),
+  });
+}
