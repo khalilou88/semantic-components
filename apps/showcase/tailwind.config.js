9fdb3298efe5f4ed5a721c742502efb0a78aee78
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-05 13:43:24 +0100

    build: work in progress

--- apps/showcase/tailwind.config.js
+++ apps/showcase/tailwind.config.js
@@ -1,15 +1,7 @@
-const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
 const { join } = require('path');
+const tailwindWorkspacePreset = require('../../tailwind-workspace-preset');
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
-    ...createGlobPatternsForDependencies(__dirname),
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-  presets: [require('../../tailwind-workspace-preset')],
+  presets: [tailwindWorkspacePreset],
+  content: [join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}')],
 };
