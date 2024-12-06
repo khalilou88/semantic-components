84cf16973c3034ae9fe7d4400a89197b9777983a
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-06 19:43:43 +0100

    build: work in progress

--- libs/ui/src/components/button/btn-primary.ts
+++ libs/ui/src/components/button/btn-primary.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'button[scBtnPrimary]',
@@ -11,7 +11,6 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
       @apply inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2 w-full;
     }
   `,
-  encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ScBtnPrimary {}
