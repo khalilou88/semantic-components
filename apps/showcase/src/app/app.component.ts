2d22a21d644f4355ba0ecb770a8ea7b5c40de1b4
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-06 18:31:33 +0100

    build: work in progress

--- apps/showcase/src/app/app.component.ts
+++ apps/showcase/src/app/app.component.ts
@@ -98,6 +98,15 @@ import { RouterModule } from '@angular/router';
           </form>
           <ul class="space-y-2">
             <li>
+              <a
+                class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                routerLink="/colors"
+              >
+                <span class="ml-3">Colors</span>
+              </a>
+            </li>
+
+            <li>
               <cdk-accordion>
                 <cdk-accordion-item #item="cdkAccordionItem">
                   <button
--- apps/showcase/src/app/pages/colors-page.ts
+++ apps/showcase/src/app/pages/colors-page.ts
@@ -4,7 +4,37 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   selector: 'app-colors-page',
   imports: [],
   template: `
-    <p>colors-page works!</p>
+    <h1>Primitive colors</h1>
+
+    <h1>Primary colors</h1>
+
+    <h2>Light mode</h2>
+
+    <div class="flex">
+      <div class="m-5">
+        <div class="border mx-2 mb-1 rounded bg-primary p-4"></div>
+        <div>Default</div>
+      </div>
+
+      <div class="m-5">
+        <div class="border mx-2 mb-1 rounded bg-primary-foreground p-4"></div>
+        <div>Foreground</div>
+      </div>
+    </div>
+
+    <h2>Dark mode</h2>
+
+    <div class="dark flex">
+      <div class="m-5">
+        <div class="border mx-2 mb-1 rounded bg-primary p-4"></div>
+        <div>Default</div>
+      </div>
+
+      <div class="m-5">
+        <div class="border mx-2 mb-1 rounded bg-primary-foreground p-4"></div>
+        <div>Foreground</div>
+      </div>
+    </div>
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
