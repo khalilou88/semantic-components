517e924643e0f6e7a9d2abd4ae9c432803cfe1f9
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-06 13:42:50 +0100

    build: work in progress

--- libs/ui/tailwind.config.js
+++ libs/ui/tailwind.config.js
@@ -1,7 +1,16 @@
+const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
 const { join } = require('path');
 const tailwindWorkspacePreset = require('../../tailwind-workspace-preset');
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: [
+    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
   presets: [tailwindWorkspacePreset],
-  content: [join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}')],
 };
