cbcdf1c8b41aba359e763db0e813a129493243be
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-07 01:55:49 +0100

    build: work in progress

--- libs/ui/src/components/button/btn-primary.ts
+++ libs/ui/src/components/button/btn-primary.ts
@@ -11,7 +11,7 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   },
   styles: `
     .btn-primary {
-      @apply inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2 w-full;
+      @apply inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2;
     }
   `,
   encapsulation: ViewEncapsulation.None,
