a2b38df2b750f6d0041293d038f555958d3bf5c3
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-07 02:07:45 +0100

    build: work in progress

--- libs/ui/src/components/button/btn-secondary.ts
+++ libs/ui/src/components/button/btn-secondary.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'sc-btn-secondary',
+  selector: 'button[sc-btn-secondary]',
   imports: [],
   template: `
     <ng-content></ng-content>
