c327819fc7f57caea3fc43577d6aed746da11eda
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-08 14:43:07 +0100

    build: work in progress

--- apps/showcase/src/app/pages/tooltip-page.ts
+++ apps/showcase/src/app/pages/tooltip-page.ts
@@ -1,34 +1,41 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
-import { ScTooltip } from '@semantic-components/ui';
+import { ScButton, ScTooltip } from '@semantic-components/ui';
 
 @Component({
   selector: 'app-tooltip-page',
-  imports: [ScTooltip],
+  imports: [ScTooltip, ScButton],
   template: `
-    <button
-      class="cursor-pointer rounded p-1.5 text-gray-500 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
-      type="button"
-      scTooltip="Toggle italic"
-    >
-      <svg
-        class="size-5"
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <path
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="m8.874 19 6.143-14M6 19h6.33m-.66-14H18"
-        />
-      </svg>
-      <span class="sr-only">Italic</span>
+    <button sc-button variant="outline" scTooltip="Add to library">Hover</button>
+
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+
+    <button sc-button variant="outline" scTooltip="Tooltip above" position="above">
+      Tooltip above
+    </button>
+
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+
+    <button sc-button variant="outline" scTooltip="Tooltip left" position="left">
+      Tooltip left
+    </button>
+
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+
+    <button sc-button variant="outline" scTooltip="Tooltip right" position="right">
+      Tooltip right
     </button>
   `,
   styles: ``,
