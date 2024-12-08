2e59330fb7982ade4d489d318de5a4b2d806bf66
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-08 11:47:18 +0100

    build: work in progress

--- libs/ui/src/components/select/option.ts
+++ libs/ui/src/components/select/option.ts
@@ -6,15 +6,19 @@ import {
   output,
 } from '@angular/core';
 
+import { SvgCheckIcon } from '@semantic-icons/lucide-icons';
+
 @Component({
   selector: 'sc-option',
-  imports: [],
+  imports: [SvgCheckIcon],
   template: `
     <button
       class="relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50"
       (click)="select()"
       type="button"
     >
+      <svg-check-icon class="h-4 w-4" />
+
       <ng-content />
     </button>
   `,
--- libs/ui/src/components/select/select.ts
+++ libs/ui/src/components/select/select.ts
@@ -10,7 +10,8 @@ import { SvgChevronDownIcon } from '@semantic-icons/lucide-icons';
     <button
       class="flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1"
       [cdkMenuTriggerFor]="options"
-      type="menu"
+      type="button"
+      role="combobox"
     >
       {{ placeholder() }}
       <svg-chevron-down-icon class="h-4 w-4 opacity-50" />
