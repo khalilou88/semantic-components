f04984506880f6a66409b70b7d41547950d96e1f
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-08 16:54:22 +0100

    build: work in progress

--- apps/showcase/src/app/pages/nav-page.ts
+++ apps/showcase/src/app/pages/nav-page.ts
@@ -1,12 +1,27 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
-import { NavComponent } from '@semantic-components/ui';
+import { NavComponent, ScNavLink } from '@semantic-components/ui';
 
 @Component({
   selector: 'app-nav-page',
-  imports: [NavComponent],
+  imports: [NavComponent, ScNavLink, RouterLink],
   template: `
     <sc-nav />
+
+    <nav>
+      <ul>
+        <li>
+          <a sc-nav-link routerLink="home">Home</a>
+        </li>
+        <li>
+          <a sc-nav-link routerLink="about">About</a>
+        </li>
+        <li>
+          <a sc-nav-link routerLink="shop">Shop</a>
+        </li>
+      </ul>
+    </nav>
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
--- libs/ui/src/components/nav/index.ts
+++ libs/ui/src/components/nav/index.ts
@@ -1,3 +1,4 @@
+export * from './nav-link';
 export * from './nav.component';
 export * from './nav-item.component';
 export * from './nav-item-dropdown.component';
