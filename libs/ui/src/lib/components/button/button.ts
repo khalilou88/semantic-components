48bb55dccb4e50958d25f2acf825ee0405ddd26d
Khalil LAGRIDA <32600911+khalilou88@users.noreply.github.com>
2024-12-09 21:59:39 +0100

    build: work in progress (#32)

--- libs/ui/src/lib/components/button/button.ts
+++ libs/ui/src/lib/components/button/button.ts
@@ -1,4 +1,40 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation, input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+  computed,
+  input,
+} from '@angular/core';
+
+import { cva } from 'class-variance-authority';
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+const buttonVariants = cva(
+  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+  {
+    variants: {
+      variant: {
+        primary: 'bg-primary text-primary-foreground hover:bg-primary/90',
+        destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
+        outline: 'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
+        secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
+        ghost: 'hover:bg-accent hover:text-accent-foreground',
+        link: 'text-primary underline-offset-4 hover:underline',
+      },
+      size: {
+        default: 'h-10 px-4 py-2',
+        sm: 'h-9 rounded-md px-3',
+        lg: 'h-11 rounded-md px-8',
+        icon: 'h-10 w-10',
+      },
+    },
+    defaultVariants: {
+      variant: 'primary',
+      size: 'default',
+    },
+  },
+);
 
 @Component({
   selector: 'button[sc-button], a[sc-button]',
@@ -7,53 +43,9 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation, input } from '@a
     <ng-content />
   `,
   host: {
-    '[class.sc-button]': 'true',
-    '[class.sc-button-primary]': 'variant() === "primary"',
-    '[class.sc-button-secondary]': 'variant() === "secondary"',
-    '[class.sc-button-destructive]': 'variant() === "destructive"',
-    '[class.sc-button-outline]': 'variant() === "outline"',
-    '[class.sc-button-ghost]': 'variant() === "ghost"',
-    '[class.sc-button-link]': 'variant() === "link"',
-    '[class.sc-button-size-default]': 'size() === "default"',
-    '[class.sc-button-size-sm]': 'size() === "sm"',
-    '[class.sc-button-size-lg]': 'size() === "lg"',
-    '[class.sc-button-size-icon]': 'size() === "icon"',
+    '[class]': 'classes()',
   },
-  styles: `
-    .sc-button {
-      @apply inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0;
-    }
-    .sc-button-primary {
-      @apply bg-primary text-primary-foreground hover:bg-primary/90;
-    }
-    .sc-button-secondary {
-      @apply bg-secondary text-secondary-foreground hover:bg-secondary/80;
-    }
-    .sc-button-destructive {
-      @apply bg-destructive text-destructive-foreground hover:bg-destructive/90;
-    }
-    .sc-button-outline {
-      @apply border border-input bg-background hover:bg-accent hover:text-accent-foreground;
-    }
-    .sc-button-ghost {
-      @apply hover:bg-accent hover:text-accent-foreground;
-    }
-    .sc-button-link {
-      @apply text-primary underline-offset-4 hover:underline;
-    }
-    .sc-button-size-default {
-      @apply h-10 px-4 py-2;
-    }
-    .sc-button-size-sm {
-      @apply h-9 rounded-md px-3;
-    }
-    .sc-button-size-lg {
-      @apply h-11 rounded-md px-8;
-    }
-    .sc-button-size-icon {
-      @apply h-10 w-10;
-    }
-  `,
+  styles: ``,
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -63,4 +55,14 @@ export class ScButton {
   );
 
   size = input<'default' | 'sm' | 'lg' | 'icon'>('default');
+
+  class = input<string>('');
+
+  classes = computed(() =>
+    cv(buttonVariants({ variant: this.variant(), size: this.size() }), this.class()),
+  );
+}
+
+export function cv(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
