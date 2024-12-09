389074da5b2ee6c24e2fdb337fa961a067659af7
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-09 18:03:19 +0100

    build: work in progress

--- libs/ui/src/lib/components/date-picker/month-year-header.ts
+++ libs/ui/src/lib/components/date-picker/month-year-header.ts
@@ -6,66 +6,46 @@ import {
   output,
 } from '@angular/core';
 
+import { SvgChevronLeftIcon, SvgChevronRightIcon } from '@semantic-icons/lucide-icons';
+
+import { ScButton } from '../button';
+
 @Component({
   selector: 'sc-month-year-header',
-  imports: [],
+  imports: [SvgChevronRightIcon, SvgChevronLeftIcon, ScButton],
   template: `
-    <div class="mb-2 flex justify-between">
-      <button
-        class="rounded-lg bg-white p-2.5 text-lg text-gray-500 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:hover:text-white"
-        (click)="monthYearChange.emit(-1)"
-        aria-label="Previous month"
-        type="button"
-      >
-        <svg
-          class="size-4 text-gray-800 rtl:rotate-180 dark:text-white"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 14 10"
-        >
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M13 5H1m0 0 4 4M1 5l4-4"
-          ></path>
-        </svg>
-      </button>
+    <button
+      [attr.aria-label]="'Previous month'"
+      (click)="monthYearChange.emit(-1)"
+      sc-button
+      variant="outline"
+      type="button"
+      size="icon"
+    >
+      <svg-chevron-left-icon />
+    </button>
 
-      <button
-        class="rounded-lg bg-white px-5 py-2.5 text-sm font-semibold text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600"
-        type="button"
-      >
-        {{ monthYear() }}
-      </button>
+    <button sc-button variant="ghost" type="button">
+      {{ monthYear() }}
+    </button>
 
-      <button
-        class="rounded-lg bg-white p-2.5 text-lg text-gray-500 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:hover:text-white"
-        (click)="monthYearChange.emit(1)"
-        type="button"
-        aria-label="Next month"
-      >
-        <svg
-          class="size-4 text-gray-800 rtl:rotate-180 dark:text-white"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 14 10"
-        >
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M1 5h12m0 0L9 1m4 4L9 9"
-          ></path>
-        </svg>
-      </button>
-    </div>
+    <button
+      [attr.aria-label]="'Next month'"
+      (click)="monthYearChange.emit(1)"
+      sc-button
+      variant="outline"
+      type="button"
+      size="icon"
+      type="button"
+    >
+      <svg-chevron-right-icon />
+    </button>
   `,
-  styles: ``,
+  styles: `
+    .sc-month-year-header {
+      @apply flex justify-between;
+    }
+  `,
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
