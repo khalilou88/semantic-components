1b214f90c124f6dbcdbb7df60dff057d0600f8dc
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-10 14:34:08 +0100

    build: work in progress

--- libs/ui/src/lib/components/editor/extensions/extension-color.ts
+++ libs/ui/src/lib/components/editor/extensions/extension-color.ts
@@ -1,6 +1,8 @@
 import { CdkMenu, CdkMenuItem, CdkMenuTrigger } from '@angular/cdk/menu';
 import { ChangeDetectionStrategy, Component, ViewEncapsulation, inject } from '@angular/core';
 
+import { SvgPaletteIcon } from '@semantic-icons/lucide-icons';
+
 import { ScTooltip } from '../../tooltip';
 import { colors } from '../colors';
 import { ScEditor } from '../editor';
@@ -8,7 +10,7 @@ import { ScExtensions } from './extensions';
 
 @Component({
   selector: 'sc-extension-color',
-  imports: [CdkMenuTrigger, CdkMenu, CdkMenuItem, ScTooltip],
+  imports: [CdkMenuTrigger, CdkMenu, CdkMenuItem, ScTooltip, SvgPaletteIcon],
   template: `
     <button
       class="cursor-pointer rounded p-1.5 text-gray-500 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
@@ -16,22 +18,7 @@ import { ScExtensions } from './extensions';
       type="button"
       scTooltip="Text color"
     >
-      <svg
-        class="size-5"
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        width="25"
-        height="24"
-        fill="none"
-        viewBox="0 0 25 24"
-      >
-        <path
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-width="2"
-          d="m6.532 15.982 1.573-4m-1.573 4h-1.1m1.1 0h1.65m-.077-4 2.725-6.93a.11.11 0 0 1 .204 0l2.725 6.93m-5.654 0H8.1m.006 0h5.654m0 0 .617 1.569m5.11 4.453c0 1.102-.854 1.996-1.908 1.996-1.053 0-1.907-.894-1.907-1.996 0-1.103 1.907-4.128 1.907-4.128s1.909 3.025 1.909 4.128Z"
-        />
-      </svg>
+      <svg-palette-icon />
       <span class="sr-only">Text color</span>
     </button>
 
