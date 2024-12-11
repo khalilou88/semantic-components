915f38c4d8125f0e64fd8c8acba06a79bf15880c
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-11 14:34:06 +0100

    build: work in progress

--- apps/showcase/src/app/components/header.ts
+++ apps/showcase/src/app/components/header.ts
@@ -1,11 +1,11 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation, signal } from '@angular/core';
 
 import { ScButton, ScThemeToggler } from '@semantic-components/ui';
-import { SvgGithubIcon } from '@semantic-icons/lucide-icons';
+import { SvgGithubIcon, SvgMenuIcon } from '@semantic-icons/lucide-icons';
 
 @Component({
   selector: 'app-header',
-  imports: [ScThemeToggler, SvgGithubIcon, ScButton],
+  imports: [ScThemeToggler, SvgGithubIcon, ScButton, SvgMenuIcon],
   template: `
     <div class="">
       <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -13,49 +13,34 @@ import { SvgGithubIcon } from '@semantic-icons/lucide-icons';
           <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
             <!-- Mobile menu button-->
             <button
-              class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              (click)="toggle()"
+              sc-button
+              variant="ghost"
+              size="icon"
               type="button"
               aria-controls="mobile-menu"
               aria-expanded="false"
             >
               <span class="absolute -inset-0.5"></span>
               <span class="sr-only">Open main menu</span>
-              <!--
-            Icon when menu is closed.
 
-            Menu open: "hidden", Menu closed: "block"
-          -->
-              <svg
-                class="block size-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-                data-slot="icon"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                />
-              </svg>
-              <!--
-            Icon when menu is open.
+              @if (open()) {
+                <svg
+                  class=" size-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                  data-slot="icon"
+                >
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                </svg>
+              }
 
-            Menu open: "block", Menu closed: "hidden"
-          -->
-              <svg
-                class="hidden size-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-                data-slot="icon"
-              >
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-              </svg>
+              @if (!open()) {
+                <svg-menu-icon />
+              }
             </button>
           </div>
           <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
@@ -164,4 +149,10 @@ import { SvgGithubIcon } from '@semantic-icons/lucide-icons';
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Header {}
+export class Header {
+  open = signal<boolean>(false);
+
+  toggle() {
+    this.open.update((value) => !value);
+  }
+}
