287f7e226be128fc0c83d412957577a72562e190
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-11 16:45:23 +0100

    build: work in progress

--- libs/ui/src/lib/components/sidebar/sidebar-layout.ts
+++ libs/ui/src/lib/components/sidebar/sidebar-layout.ts
@@ -1,9 +1,12 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'sc-sidebar-layout',
-  imports: [],
-  template: ``,
+  imports: [RouterModule],
+  template: `
+    <router-outlet></router-outlet>
+  `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
