15d412e04056eea4fed55b7f829a5d0e4b34e60c
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-12 16:26:31 +0100

    build: work in progress

---
+++ libs/ui/src/lib/components/layouts/container.ts
@@ -0,0 +1,29 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+  computed,
+  input,
+} from '@angular/core';
+
+import { cn } from '../../utils';
+
+@Component({
+  selector: 'div[sc-container]',
+  imports: [],
+  template: `
+    <ng-content />
+  `,
+  host: {
+    '[class]': 'classes()',
+  },
+  styles: ``,
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class scContainer {
+  class = input<string>('');
+
+  //TODO add more cases
+  classes = computed(() => cn('container', this.class()));
+}
