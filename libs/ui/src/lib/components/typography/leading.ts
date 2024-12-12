50e3dfc77428447657d4dedea237919bc5ed857b
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-12 16:38:51 +0100

    build: work in progress

--- libs/ui/src/lib/components/typography/leading.ts
+++ libs/ui/src/lib/components/typography/leading.ts
@@ -1,13 +1,50 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+  computed,
+  input,
+} from '@angular/core';
+
+import { VariantProps, cva } from 'class-variance-authority';
+
+import { cn } from '../../utils';
+
+const scLeadingVariants = cva('', {
+  variants: {
+    level: {
+      h1: '',
+      h2: '',
+      h3: '',
+      h4: '',
+      h5: '',
+      h6: '',
+    },
+  },
+  defaultVariants: {
+    level: 'h1',
+  },
+});
+
+type ScLeadingVariants = VariantProps<typeof scLeadingVariants>;
 
 @Component({
   selector: 'sc-leading',
   imports: [],
   template: `
-    <p>leading works!</p>
+    <ng-content />
   `,
+  host: {
+    '[class]': 'classes()',
+  },
   styles: ``,
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ScLeading {}
+export class ScLeading {
+  level = input<ScLeadingVariants['level']>('h1');
+
+  class = input<string>('');
+
+  classes = computed(() => cn(scLeadingVariants({ level: this.level() }), this.class()));
+}
