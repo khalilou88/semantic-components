5acd7a6daecaa7220fff4c9f977e54157a0c15d5
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-13 00:52:38 +0100

    build: work in progress

--- apps/showcase/src/app/pages/slider-page.ts
+++ apps/showcase/src/app/pages/slider-page.ts
@@ -19,6 +19,47 @@ import { ScSlider } from '@semantic-components/ui';
     <br />
 
     {{ sliderForm.value | json }}
+
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+
+    <span
+      class="relative flex touch-none select-none items-center w-[60%]"
+      dir="ltr"
+      data-orientation="horizontal"
+      aria-disabled="false"
+      style="--radix-slider-thumb-transform: translateX(-50%);"
+    >
+      <span
+        class="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary"
+        data-orientation="horizontal"
+      >
+        <span
+          class="absolute h-full bg-primary"
+          data-orientation="horizontal"
+          style="left: 0%; right: 66%;"
+        ></span>
+      </span>
+      <span
+        style="transform: var(--radix-slider-thumb-transform); position: absolute; left: calc(34% + 3.2px);"
+      >
+        <span
+          class="block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+          role="slider"
+          aria-valuemin="0"
+          aria-valuemax="100"
+          aria-orientation="horizontal"
+          data-orientation="horizontal"
+          tabindex="0"
+          data-radix-collection-item=""
+          aria-valuenow="34"
+          style=""
+        ></span>
+      </span>
+    </span>
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
