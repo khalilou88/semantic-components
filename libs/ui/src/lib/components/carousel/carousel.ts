5757f2e985d5205d664845145b538f9e78861b33
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-13 12:10:10 +0100

    build: work in progress

--- libs/ui/src/lib/components/carousel/carousel.ts
+++ libs/ui/src/lib/components/carousel/carousel.ts
@@ -1,10 +1,15 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  ElementRef,
   ViewEncapsulation,
+  afterNextRender,
   computed,
+  inject,
   input,
 } from '@angular/core';
+
+import EmblaCarousel from 'embla-carousel';
 
 import { cn } from '../../utils';
 
@@ -27,6 +32,17 @@ export class ScCarousel {
   class = input<string>('');
 
   classes = computed(() => cn('relative', this.class()));
+
+  private emblaNode = inject(ElementRef);
+
+  constructor() {
+    afterNextRender(() => {
+      const options = { loop: false };
+      const emblaApi = EmblaCarousel(this.emblaNode.nativeElement, options);
+
+      console.log(emblaApi.slideNodes()); // Access API
+    });
+  }
 
   handleKeyDown() {
     console.log('handleKeyDown');
