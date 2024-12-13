60cdc818c3c42eda1f5f6ee10da8a8da44030b9c
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-13 20:36:38 +0100

    build: work in progress

--- libs/ui/src/lib/components/checkbox/checkbox.ts
+++ libs/ui/src/lib/components/checkbox/checkbox.ts
@@ -1,30 +1,21 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  ViewContainerRef,
   ViewEncapsulation,
+  afterNextRender,
   computed,
   input,
 } from '@angular/core';
 
 import { cn } from '../../utils';
+import { SVG } from './svg';
 
 @Component({
   selector: 'input[sc-checkbox]',
   imports: [],
   template: `
     <ng-content />
-    <svg
-      class="absolute w-4 h-4 pointer-events-none hidden peer-checked:block stroke-white mt-1 outline-none"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="4"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <polyline points="20 6 9 17 4 12"></polyline>
-    </svg>
   `,
   host: {
     '[class]': 'classes()',
@@ -42,4 +33,10 @@ export class ScCheckbox {
       this.class(),
     ),
   );
+
+  constructor(private viewContainer: ViewContainerRef) {
+    afterNextRender(() => {
+      this.viewContainer.createComponent(SVG);
+    });
+  }
 }
---
+++ libs/ui/src/lib/components/checkbox/svg.ts
@@ -0,0 +1,43 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+  computed,
+  input,
+} from '@angular/core';
+
+import { cn } from '../../utils';
+
+@Component({
+  selector: 'sc-svg',
+  imports: [],
+  template: `
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="4"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <polyline points="20 6 9 17 4 12"></polyline>
+    </svg>
+  `,
+  host: {
+    '[class]': 'classes()',
+  },
+  styles: ``,
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SVG {
+  class = input<string>('');
+
+  classes = computed(() =>
+    cn(
+      'absolute w-4 h-4 pointer-events-none hidden peer-checked:block stroke-white mt-1 outline-none',
+      this.class(),
+    ),
+  );
+}
