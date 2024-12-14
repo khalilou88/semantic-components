9b8735631e56ccbd42e476d7e10f6d395a38ed31
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-14 15:42:22 +0100

    build: work in progress

--- libs/ui/src/lib/components/checkbox/checkbox.ts
+++ libs/ui/src/lib/components/checkbox/checkbox.ts
@@ -20,16 +20,14 @@ import { cn } from '../../utils';
   imports: [SvgCheckIcon],
   template: `
     <input
-      class="peer relative appearance-none h-4 w-4 shrink-0 cursor-pointer rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground"
+      [class]="checkboxClasses()"
       [disabled]="disabled()"
       [attr.data-state]="state()"
       type="checkbox"
     />
 
     @if (checked() === true) {
-      <svg-check-icon
-        class="absolute w-4 h-4 outline-none cursor-pointer text-primary-foreground"
-      />
+      <svg-check-icon [class]="svgClasses()" />
     }
   `,
   host: {
@@ -53,6 +51,21 @@ export class ScCheckbox implements ControlValueAccessor {
   class = input<string>('');
 
   classes = computed(() => cn('flex', this.class()));
+
+  checkboxClass = input<string>('');
+
+  checkboxClasses = computed(() =>
+    cn(
+      'peer relative appearance-none h-4 w-4 shrink-0 cursor-pointer rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground',
+      this.checkboxClass(),
+    ),
+  );
+
+  svgClass = input<string>('');
+
+  svgClasses = computed(() =>
+    cn('absolute w-4 h-4 outline-none cursor-pointer text-primary-foreground', this.svgClass()),
+  );
 
   checked = model<boolean>(false);
 
