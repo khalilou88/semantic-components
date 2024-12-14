d60254434682afd53482f00358e69450bab892aa
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-14 03:06:07 +0100

    build: work in progress

--- libs/ui/src/lib/components/checkbox/checkbox.ts
+++ libs/ui/src/lib/components/checkbox/checkbox.ts
@@ -16,14 +16,14 @@ import { cn } from '../../utils';
   imports: [SvgCheckIcon],
   template: `
     <input
-      class="peer relative appearance-none h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground"
+      class="peer relative appearance-none h-4 w-4 shrink-0 cursor-pointer rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground"
       [attr.data-state]="state()"
       type="checkbox"
     />
 
     @if (checked() === true) {
       <svg-check-icon
-        class="absolute w-4 h-4 pointer-events-none data-[state=checked]:text-primary-foreground outline-none"
+        class="absolute w-4 h-4 cursor-pointer data-[state=checked]:text-primary-foreground outline-none"
         [attr.data-state]="state()"
       />
     }
