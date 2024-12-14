57e57a38bb32da533a9b6ff93aa97183d11cc919
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-14 02:36:16 +0100

    build: work in progress

--- apps/showcase/src/app/pages/checkbox-page.ts
+++ apps/showcase/src/app/pages/checkbox-page.ts
@@ -6,13 +6,8 @@ import { ScCheckbox } from '@semantic-components/ui';
   selector: 'app-checkbox-page',
   imports: [ScCheckbox],
   template: `
-    <input sc-checkbox type="checkbox" />
+    <sc-checkbox />
 
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
     <br />
     <br />
     <br />
@@ -26,36 +21,6 @@ import { ScCheckbox } from '@semantic-components/ui';
       data-state="unchecked"
       value="on"
     ></button>
-
-    <br />
-    <br />
-    <br />
-    <br />
-
-    <br />
-    <br />
-    <br />
-    <br />
-
-    <div class="w-full flex gap-2">
-      <input
-        class="peer relative appearance-none shrink-0 w-4 h-4 border-2 border-blue-200 rounded-sm mt-1 bg-white focus:outline-none focus:ring-offset-0 focus:ring-1 focus:ring-blue-100 checked:bg-blue-500 checked:border-0 disabled:border-steel-400 disabled:bg-steel-400"
-        type="checkbox"
-      />
-      <svg
-        class="absolute w-4 h-4 pointer-events-none hidden peer-checked:block stroke-white mt-1 outline-none"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="4"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <polyline points="20 6 9 17 4 12"></polyline>
-      </svg>
-      <label>My label</label>
-    </div>
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
--- libs/ui/src/lib/components/checkbox/checkbox.ts
+++ libs/ui/src/lib/components/checkbox/checkbox.ts
@@ -1,23 +1,29 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  ViewContainerRef,
   ViewEncapsulation,
-  afterNextRender,
   computed,
-  effect,
   input,
   signal,
 } from '@angular/core';
 
+import { SvgCheckIcon } from '@semantic-icons/lucide-icons';
+
 import { cn } from '../../utils';
-import { SVG } from './svg';
 
 @Component({
-  selector: 'input[sc-checkbox]',
-  imports: [],
+  selector: 'sc-checkbox',
+  imports: [SvgCheckIcon],
   template: `
-    <ng-content />
+    <div class="w-full flex gap-2">
+      <input
+        class="peer relative appearance-none shrink-0 w-4 h-4 border-2 border-blue-200 rounded-sm mt-1 bg-white focus:outline-none focus:ring-offset-0 focus:ring-1 focus:ring-blue-100 checked:bg-blue-500 checked:border-0 disabled:border-steel-400 disabled:bg-steel-400"
+        type="checkbox"
+      />
+      <svg-check-icon
+        class="absolute w-4 h-4 pointer-events-none hidden peer-checked:block stroke-white mt-1 outline-none"
+      />
+    </div>
   `,
   host: {
     '[class]': 'classes()',
@@ -38,20 +44,6 @@ export class ScCheckbox {
   );
 
   checked = signal<boolean>(false);
-
-  constructor(private viewContainer: ViewContainerRef) {
-    effect(() => {
-      console.log('yyyyy');
-
-      if (this.checked() === true) {
-        this.viewContainer.createComponent(SVG);
-      } else {
-        this.viewContainer.detach();
-      }
-    });
-
-    afterNextRender(() => {});
-  }
 
   toggle() {
     this.checked.update((v) => !v);
