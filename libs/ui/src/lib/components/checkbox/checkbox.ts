ba18881b9c43a050b6d5779028505423cc6d37a8
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-14 02:37:11 +0100

    build: work in progress

--- libs/ui/src/lib/components/checkbox/checkbox.ts
+++ libs/ui/src/lib/components/checkbox/checkbox.ts
@@ -36,12 +36,7 @@ import { cn } from '../../utils';
 export class ScCheckbox {
   class = input<string>('');
 
-  classes = computed(() =>
-    cn(
-      'flex peer appearance-none shrink-0 w-4 h-4 border-2 border-blue-200 rounded-sm bg-white focus:outline-none focus:ring-offset-0 focus:ring-1 focus:ring-blue-100 checked:bg-blue-500 checked:border-0 disabled:border-steel-400 disabled:bg-steel-400',
-      this.class(),
-    ),
-  );
+  classes = computed(() => cn('', this.class()));
 
   checked = signal<boolean>(false);
 
