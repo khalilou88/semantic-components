2fbff24fd58526177fb3b4e593027ad3858c241d
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-15 13:49:56 +0100

    build: work in progress

--- apps/showcase/src/app/pages/accordion-page.ts
+++ apps/showcase/src/app/pages/accordion-page.ts
@@ -12,29 +12,29 @@ import {
   imports: [ScAccordion, ScAccordionItem, ScAccordionContent, ScAccordionItemDescription],
   template: `
     <div class="preview flex min-h-[350px] w-full justify-center p-10 items-center">
-      <div class="w-full" sc-accordion>
-        <div sc-accordion-item>
+      <sc-accordion class="w-full">
+        <sc-accordion-item>
           <sc-accordion-item-description>Is it accessible?</sc-accordion-item-description>
 
           <sc-accordion-content>
             Yes. It adheres to the WAI-ARIA design pattern.
           </sc-accordion-content>
-        </div>
-        <div sc-accordion-item>
+        </sc-accordion-item>
+        <sc-accordion-item>
           <sc-accordion-item-description>Is it styled?</sc-accordion-item-description>
 
           <sc-accordion-content>
             Yes. It comes with default styles that matches the other components&apos; aesthetic.
           </sc-accordion-content>
-        </div>
-        <div sc-accordion-item>
+        </sc-accordion-item>
+        <sc-accordion-item>
           <sc-accordion-item-description>Is it animated?</sc-accordion-item-description>
 
           <sc-accordion-content>
             Yes. It&apos;s animated by default, but you can disable it if you prefer.
           </sc-accordion-content>
-        </div>
-      </div>
+        </sc-accordion-item>
+      </sc-accordion>
     </div>
   `,
   styles: ``,
