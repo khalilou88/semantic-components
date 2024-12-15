ba85cfbce06ae46fa0c6fbaa774808afd49bb617
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-15 14:40:56 +0100

    build: work in progress

--- apps/showcase/src/app/pages/scroll-area-page.ts
+++ apps/showcase/src/app/pages/scroll-area-page.ts
@@ -4,7 +4,7 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   selector: 'app-scroll-area-page',
   imports: [],
   template: `
-    <div class="m-10 flex">
+    <main class="relative py-6 lg:gap-10 lg:py-8 xl:grid xl:grid-cols-[1fr_300px]">
       <div class="mx-auto w-full min-w-0 max-w-3xl">
         <div class="mb-4 flex items-center space-x-1 text-sm leading-none text-muted-foreground">
           <div class="truncate">Docs</div>
@@ -833,7 +833,7 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
           </div>
         </div>
       </div>
-    </div>
+    </main>
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
