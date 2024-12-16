45d3c39e8764cf568441271c25af42a2b495da95
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-16 17:16:46 +0100

    build: work in progress

--- apps/showcase/src/app/pages/switch-page.ts
+++ apps/showcase/src/app/pages/switch-page.ts
@@ -1,10 +1,37 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
+import { ScSwitch } from '@semantic-components/ui';
+
 @Component({
   selector: 'app-switch-page',
-  imports: [],
+  imports: [ScSwitch],
   template: `
-    <p>switch-page works!</p>
+    <sc-switch />
+
+    <div class="preview flex min-h-[350px] w-full justify-center p-10 items-center">
+      <div class="flex items-center space-x-2">
+        <button
+          class="peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input"
+          id="airplane-mode"
+          type="button"
+          role="switch"
+          aria-checked="false"
+          data-state="unchecked"
+          value="on"
+        >
+          <span
+            class="pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0"
+            data-state="unchecked"
+          ></span>
+        </button>
+        <label
+          class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          for="airplane-mode"
+        >
+          Airplane Mode
+        </label>
+      </div>
+    </div>
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
--- libs/ui/src/lib/components/switch/switch.ts
+++ libs/ui/src/lib/components/switch/switch.ts
@@ -1,13 +1,41 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+  computed,
+  input,
+} from '@angular/core';
+
+import { cn } from '../../utils';
 
 @Component({
   selector: 'sc-switch',
   imports: [],
   template: `
-    <p>switch works!</p>
+    <input
+      class="peer appearance-none w-11 h-5 bg-slate-100 rounded-full checked:bg-slate-800 cursor-pointer transition-colors duration-300"
+      id="switch-component"
+      checked
+      type="checkbox"
+    />
+    <label
+      class="absolute top-0 left-0 w-5 h-5 bg-white rounded-full border border-slate-300 shadow-sm transition-transform duration-300 peer-checked:translate-x-6 peer-checked:border-slate-800 cursor-pointer"
+      for="switch-component"
+    ></label>
   `,
+  host: {
+    '[class]': 'hostClasses()',
+  },
   styles: ``,
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ScSwitch {}
+export class ScSwitch {
+  hostClass = input<string>('');
+
+  hostClasses = computed(() => cn('relative inline-block w-11 h-5', this.hostClass()));
+
+  class = input<string>('');
+
+  classes = computed(() => cn('', this.class()));
+}
