1024755df2898019869c7268e6fc6333fad9c6c1
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-16 17:55:45 +0100

    build: work in progress

--- apps/showcase/src/app/pages/switch-page.ts
+++ apps/showcase/src/app/pages/switch-page.ts
@@ -1,41 +1,31 @@
+import { JsonPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 import { ScSwitch } from '@semantic-components/ui';
 
 @Component({
   selector: 'app-switch-page',
-  imports: [ScSwitch],
+  imports: [ScSwitch, ReactiveFormsModule, JsonPipe],
   template: `
     <br />
     <br />
     <br />
+    <form [formGroup]="switchForm">
+      <sc-switch formControlName="switch">Airplane Mode</sc-switch>
+    </form>
 
-    <sc-switch>Airplane Mode</sc-switch>
-
-    <div class="preview flex min-h-[350px] w-full justify-center p-10 items-center">
-      <div class="flex items-center space-x-2">
-        <button
-          class="peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input"
-          id="airplane-mode"
-          type="button"
-          role="switch"
-          aria-checked="false"
-          data-state="unchecked"
-          value="on"
-        >
-          <span data-state="unchecked"></span>
-        </button>
-        <label
-          class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-          for="airplane-mode"
-        >
-          Airplane Mode
-        </label>
-      </div>
-    </div>
+    <br />
+    <br />
+    <br />
+    {{ switchForm.value | json }}
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class SwitchPage {}
+export default class SwitchPage {
+  switchForm = new FormGroup({
+    switch: new FormControl(),
+  });
+}
