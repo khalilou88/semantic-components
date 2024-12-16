c8d5355d49bd236108ab90661c8f97ae48f30bc0
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-16 22:45:38 +0100

    build: work in progress

--- libs/ui/src/lib/components/input-otp/input-otp.ts
+++ libs/ui/src/lib/components/input-otp/input-otp.ts
@@ -4,11 +4,23 @@ import {
   ViewEncapsulation,
   booleanAttribute,
   computed,
+  forwardRef,
   input,
+  model,
   numberAttribute,
+  signal,
   viewChildren,
 } from '@angular/core';
-import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import {
+  ControlValueAccessor,
+  FormArray,
+  FormControl,
+  FormGroup,
+  NG_VALUE_ACCESSOR,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 import { cn } from '../../utils';
 import { ScInputOTPSlot } from './input-otp-slot';
@@ -24,8 +36,15 @@ import { ScInputOTPSlot } from './input-otp-slot';
   },
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => ScInputOtp),
+      multi: true,
+    },
+  ],
 })
-export class ScInputOtp {
+export class ScInputOtp implements ControlValueAccessor {
   class = input<string>('');
 
   classes = computed(() => cn('flex items-center gap-2 has-[:disabled]:opacity-50', this.class()));
@@ -53,5 +72,57 @@ export class ScInputOtp {
       this.slots()[i].control = control;
       this.inputs.push(control);
     }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    this.formGroup.valueChanges.pipe(takeUntilDestroyed()).subscribe((value) => {
+      const tel = this.formGroup.valid ? this.inputs.value.join('') : null;
+      this._updateValue(tel);
+    });
+  }
+
+  private _updateValue(tel: string | null) {
+    // const current = this._value();
+    // if (
+    //   tel === current ||
+    //   (tel?.area === current?.area &&
+    //     tel?.exchange === current?.exchange &&
+    //     tel?.subscriber === current?.subscriber)
+    // ) {
+    //   return;
+    // }
+    this._value.set(tel);
+  }
+
+  readonly _value = model<string | null>(null, { alias: 'value' });
+
+  readonly _disabledByInput = input<boolean, unknown>(false, {
+    alias: 'disabled',
+    transform: booleanAttribute,
+  });
+
+  private readonly _disabledByCva = signal(false);
+  private readonly _disabled = computed(() => this._disabledByInput() || this._disabledByCva());
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
+  onChange = (_: any) => {};
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  onTouched = () => {};
+
+  writeValue(tel: string | null): void {
+    this._updateValue(tel);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this._disabledByCva.set(isDisabled);
   }
 }
