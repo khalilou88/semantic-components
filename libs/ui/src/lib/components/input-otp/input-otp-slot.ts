53a118bc9ec44de17612c59b595c4d4b53a80205
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-16 22:04:29 +0100

    build: work in progress

--- libs/ui/src/lib/components/input-otp/input-otp-slot.ts
+++ libs/ui/src/lib/components/input-otp/input-otp-slot.ts
@@ -17,9 +17,11 @@ import { cn } from '../../utils';
       class="relative flex h-10 w-10 items-center justify-center border-y border-r border-input text-sm transition-all first:rounded-l-md first:border-l last:rounded-r-md"
       [ngClass]="{ 'z-10 ring-2 ring-ring ring-offset-background': isActive }"
     >
-      <div class="pointer-events-none absolute inset-0 flex items-center justify-center">
-        <div class="h-4 w-px animate-caret-blink bg-foreground duration-1000"></div>
-      </div>
+      @if (hasFakeCaret) {
+        <div class="pointer-events-none absolute inset-0 flex items-center justify-center">
+          <div class="h-4 w-px animate-caret-blink bg-foreground duration-1000"></div>
+        </div>
+      }
     </div>
   `,
   host: {
@@ -33,6 +35,8 @@ export class ScInputOTPSlot {
   class = input<string>('');
 
   isActive = false;
+
+  hasFakeCaret = false;
 
   classes = computed(() => cn('', this.class()));
 }
