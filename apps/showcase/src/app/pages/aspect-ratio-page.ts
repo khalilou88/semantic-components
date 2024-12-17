16a2635bd2c764ca89ddb2f06763b8a9b8c7b1fe
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-17 23:12:13 +0100

    build: work in progress

--- apps/showcase/src/app/pages/aspect-ratio-page.ts
+++ apps/showcase/src/app/pages/aspect-ratio-page.ts
@@ -1,10 +1,41 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
+import { ScAspectRatio } from '@semantic-components/ui';
+
 @Component({
   selector: 'app-aspect-ratio-page',
-  imports: [],
+  imports: [ScAspectRatio],
   template: `
-    <p>aspect-ratio-page works!</p>
+    <div class="preview flex min-h-[350px] w-full justify-center p-10 items-center">
+      <sc-aspect-ratio class="bg-muted" ratio="16 / 9">
+        <img
+          class="h-full w-full rounded-md object-cover"
+          src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
+          alt="Photo by Drew Beamer"
+          fill
+        />
+      </sc-aspect-ratio>
+    </div>
+
+    <div class="preview flex min-h-[350px] w-full justify-center p-10 items-center">
+      <div
+        data-radix-aspect-ratio-wrapper=""
+        style="position: relative; width: 100%; padding-bottom: 56.25%;"
+      >
+        <div class="bg-muted" style="position: absolute; inset: 0px;">
+          <img
+            class="h-full w-full rounded-md object-cover"
+            alt="Photo by Drew Beamer"
+            loading="lazy"
+            decoding="async"
+            data-nimg="fill"
+            sizes="100vw"
+            src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
+            style="position: absolute; height: 100%; width: 100%; inset: 0px; color: transparent;"
+          />
+        </div>
+      </div>
+    </div>
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
--- libs/ui/src/lib/components/aspect-ratio/aspect-ratio.ts
+++ libs/ui/src/lib/components/aspect-ratio/aspect-ratio.ts
@@ -1,13 +1,31 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+  computed,
+  input,
+} from '@angular/core';
+
+import { cn } from '../../utils';
 
 @Component({
   selector: 'sc-aspect-ratio',
   imports: [],
   template: `
-    <p>aspect-ratio works!</p>
+    <div style="position: absolute; inset: 0px;">
+      <ng-content />
+    </div>
   `,
+  host: {
+    '[class]': 'classes()',
+    '[style]': '"position: relative; width: 100%; padding-bottom: 56.25%;"',
+  },
   styles: ``,
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AspectRatio {}
+export class ScAspectRatio {
+  class = input<string>('');
+
+  classes = computed(() => cn('', this.class()));
+}
