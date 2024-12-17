7e4a0b4cf5a271d45b888790ceda0e5b2b536e69
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-17 16:54:02 +0100

    build: work in progress

--- libs/ui/src/lib/components/hover-card/hover-card.ts
+++ libs/ui/src/lib/components/hover-card/hover-card.ts
@@ -4,8 +4,12 @@ import {
   Component,
   TemplateRef,
   ViewEncapsulation,
+  computed,
+  input,
   signal,
 } from '@angular/core';
+
+import { cn } from '../../utils';
 
 @Component({
   selector: 'sc-hover-card',
@@ -15,10 +19,22 @@ import {
       <ng-container *ngTemplateOutlet="templateRef()"></ng-container>
     }
   `,
+  host: {
+    '[class]': 'classes()',
+  },
   styles: ``,
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ScHoverCard {
   templateRef = signal<TemplateRef<unknown> | null>(null);
+
+  class = input<string>('');
+
+  classes = computed(() =>
+    cn(
+      'z-50 w-64 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+      this.class(),
+    ),
+  );
 }
