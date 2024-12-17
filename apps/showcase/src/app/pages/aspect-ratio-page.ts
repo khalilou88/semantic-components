0533e02ed148548419aa31143dfdfdefbf7f6e8d
Khalil LAGRIDA <32600911+khalilou88@users.noreply.github.com>
2024-12-18 00:02:14 +0100

    Aspect ratio (#50)
    * build: work in progress
    
    * build: work in progress
    
    * build: work in progress
    
    * build: work in progress
    
    * build: work in progress
    
    * build: work in progress

---
+++ apps/showcase/src/app/pages/aspect-ratio-page.ts
@@ -0,0 +1,42 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+
+import { ScAspectRatio } from '@semantic-components/ui';
+
+@Component({
+  selector: 'app-aspect-ratio-page',
+  imports: [ScAspectRatio],
+  template: `
+    <div class="preview flex min-h-[350px] w-full justify-center p-10 items-center">
+      <sc-aspect-ratio
+        wrapperClass="bg-muted"
+        ratio="16 / 9"
+        src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
+        alt="Photo by Drew Beamer"
+      />
+    </div>
+
+    <div class="preview flex min-h-[350px] w-full justify-center p-10 items-center">
+      <div
+        data-radix-aspect-ratio-wrapper=""
+        style="position: relative; width: 100%; padding-bottom: 56.25%;"
+      >
+        <div class="bg-muted" style="position: absolute; inset: 0px;">
+          <img
+            class="h-full w-full rounded-md object-cover"
+            alt="Photo by Drew Beamer"
+            loading="lazy"
+            decoding="async"
+            data-nimg="fill"
+            sizes="100vw"
+            src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
+            style="position: absolute; height: 100%; width: 100%; inset: 0px; color: transparent;"
+          />
+        </div>
+      </div>
+    </div>
+  `,
+  styles: ``,
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class AspectRatioPage {}
--- libs/ui/src/lib/components/aspect-ratio/aspect-ratio.ts
+++ libs/ui/src/lib/components/aspect-ratio/aspect-ratio.ts
@@ -1,13 +1,73 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+  booleanAttribute,
+  computed,
+  input,
+  signal,
+} from '@angular/core';
+
+import { cn } from '../../utils';
 
 @Component({
   selector: 'sc-aspect-ratio',
-  imports: [],
+  imports: [NgOptimizedImage],
   template: `
-    <p>aspect-ratio works!</p>
+    <div [class]="wrapperClass()" [style]="wrapperStyle()">
+      @if (priority()) {
+        <img
+          [class]="imageClasses()"
+          [ngSrc]="src()"
+          [alt]="alt()"
+          [style]="imageStyle()"
+          fill
+          priority
+        />
+      } @else {
+        <img [class]="imageClasses()" [ngSrc]="src()" [alt]="alt()" [style]="imageStyle()" fill />
+      }
+    </div>
   `,
+  host: {
+    '[class]': 'hostClasses()',
+    '[style]': 'hostStyle()',
+  },
   styles: ``,
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AspectRatio {}
+export class ScAspectRatio {
+  imageClass = input<string>('');
+
+  imageClasses = computed(() => cn('h-full w-full rounded-md object-cover', this.imageClass()));
+
+  imageStyle = signal(
+    'position: absolute; height: 100%; width: 100%; inset: 0px; color: transparent;',
+  );
+
+  hostClass = input<string>('');
+
+  hostClasses = computed(() => cn('h-full w-full rounded-md object-cover', this.hostClass()));
+
+  hostStyle = computed(() => {
+    const parts = this.ratio()
+      .split('/')
+      .map((s) => +s);
+
+    const paddingBottom = (parts[1] / parts[0]) * 100;
+
+    return `position: relative; width: 100%; padding-bottom: ${paddingBottom}%;`;
+  });
+
+  wrapperClass = input<string>('');
+
+  wrapperStyle = signal('position: absolute; inset: 0px;');
+
+  ratio = input<string>('1 / 1');
+  src = input.required<string>();
+  alt = input<string>('');
+
+  priority = input<boolean, unknown>(false, { transform: booleanAttribute });
+}
