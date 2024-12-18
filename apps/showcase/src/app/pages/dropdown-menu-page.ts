63d8bf06a7cc83119e741df823a7c3814ad21a37
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-18 23:12:20 +0100

    build: work in progress

--- apps/showcase/src/app/pages/dropdown-menu-page.ts
+++ apps/showcase/src/app/pages/dropdown-menu-page.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
 import {
+  ScMenuContent,
   ScMenuGroup,
   ScMenuItem,
   ScMenuLabel,
@@ -52,88 +53,91 @@ import {
     ScMenuSub,
     ScMenuSubTrigger,
     ScMenuSubContent,
+    ScMenuContent,
   ],
   template: `
     <div class="m-10">
-      <sc-menu-label>My Account</sc-menu-label>
-      <sc-menu-separator />
-      <sc-menu-group>
-        <sc-menu-item>
-          <svg-user-icon />
-          <span>Profile</span>
-          <span sc-menu-shortcut>⇧⌘P</span>
-        </sc-menu-item>
-        <sc-menu-item>
-          <svg-credit-card-icon />
-          <span>Billing</span>
-          <span sc-menu-shortcut>⌘B</span>
-        </sc-menu-item>
-        <sc-menu-item>
-          <svg-settings-icon />
-          <span>Settings</span>
-          <span sc-menu-shortcut>⌘S</span>
-        </sc-menu-item>
-        <sc-menu-item>
-          <svg-keyboard-icon />
-          <span>Keyboard shortcuts</span>
-          <span sc-menu-shortcut>⌘K</span>
-        </sc-menu-item>
-      </sc-menu-group>
-      <sc-menu-separator />
-      <sc-menu-group>
-        <sc-menu-item>
-          <svg-users-icon />
-          <span>Team</span>
-        </sc-menu-item>
-        <sc-menu-sub>
-          <sc-menu-sub-trigger>
-            <svg-user-plus-icon />
-            <span>Invite users</span>
-          </sc-menu-sub-trigger>
+      <sc-menu-content>
+        <sc-menu-label>My Account</sc-menu-label>
+        <sc-menu-separator />
+        <sc-menu-group>
+          <sc-menu-item>
+            <svg-user-icon />
+            <span>Profile</span>
+            <span sc-menu-shortcut>⇧⌘P</span>
+          </sc-menu-item>
+          <sc-menu-item>
+            <svg-credit-card-icon />
+            <span>Billing</span>
+            <span sc-menu-shortcut>⌘B</span>
+          </sc-menu-item>
+          <sc-menu-item>
+            <svg-settings-icon />
+            <span>Settings</span>
+            <span sc-menu-shortcut>⌘S</span>
+          </sc-menu-item>
+          <sc-menu-item>
+            <svg-keyboard-icon />
+            <span>Keyboard shortcuts</span>
+            <span sc-menu-shortcut>⌘K</span>
+          </sc-menu-item>
+        </sc-menu-group>
+        <sc-menu-separator />
+        <sc-menu-group>
+          <sc-menu-item>
+            <svg-users-icon />
+            <span>Team</span>
+          </sc-menu-item>
+          <sc-menu-sub>
+            <sc-menu-sub-trigger>
+              <svg-user-plus-icon />
+              <span>Invite users</span>
+            </sc-menu-sub-trigger>
 
-          <ng-template #sub_menu_1>
-            <sc-menu-sub-content>
-              <sc-menu-item>
-                <svg-mail-icon />
-                <span>Email</span>
-              </sc-menu-item>
-              <sc-menu-item>
-                <svg-message-square-icon />
-                <span>Message</span>
-              </sc-menu-item>
-              <sc-menu-separator />
-              <sc-menu-item>
-                <svg-circle-plus-icon />
-                <span>More...</span>
-              </sc-menu-item>
-            </sc-menu-sub-content>
-          </ng-template>
-        </sc-menu-sub>
+            <ng-template #sub_menu_1>
+              <sc-menu-sub-content>
+                <sc-menu-item>
+                  <svg-mail-icon />
+                  <span>Email</span>
+                </sc-menu-item>
+                <sc-menu-item>
+                  <svg-message-square-icon />
+                  <span>Message</span>
+                </sc-menu-item>
+                <sc-menu-separator />
+                <sc-menu-item>
+                  <svg-circle-plus-icon />
+                  <span>More...</span>
+                </sc-menu-item>
+              </sc-menu-sub-content>
+            </ng-template>
+          </sc-menu-sub>
+          <sc-menu-item>
+            <svg-plus-icon />
+            <span>New Team</span>
+            <span sc-menu-shortcut>⌘+T</span>
+          </sc-menu-item>
+        </sc-menu-group>
+        <sc-menu-separator />
         <sc-menu-item>
-          <svg-plus-icon />
-          <span>New Team</span>
-          <span sc-menu-shortcut>⌘+T</span>
+          <svg-github-icon />
+          <span>GitHub</span>
         </sc-menu-item>
-      </sc-menu-group>
-      <sc-menu-separator />
-      <sc-menu-item>
-        <svg-github-icon />
-        <span>GitHub</span>
-      </sc-menu-item>
-      <sc-menu-item>
-        <svg-life-buoy-icon />
-        <span>Support</span>
-      </sc-menu-item>
-      <sc-menu-item disabled>
-        <svg-cloud-icon />
-        <span>API</span>
-      </sc-menu-item>
-      <sc-menu-separator />
-      <sc-menu-item>
-        <svg-log-out-icon />
-        <span>Log out</span>
-        <span sc-menu-shortcut>⇧⌘Q</span>
-      </sc-menu-item>
+        <sc-menu-item>
+          <svg-life-buoy-icon />
+          <span>Support</span>
+        </sc-menu-item>
+        <sc-menu-item disabled>
+          <svg-cloud-icon />
+          <span>API</span>
+        </sc-menu-item>
+        <sc-menu-separator />
+        <sc-menu-item>
+          <svg-log-out-icon />
+          <span>Log out</span>
+          <span sc-menu-shortcut>⇧⌘Q</span>
+        </sc-menu-item>
+      </sc-menu-content>
     </div>
   `,
   styles: `
