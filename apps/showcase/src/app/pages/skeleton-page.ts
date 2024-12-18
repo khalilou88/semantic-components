274814d490b84bc8d4100fb5b4a25d310b7e0320
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-18 16:47:56 +0100

    build: work in progress

--- apps/showcase/src/app/pages/skeleton-page.ts
+++ apps/showcase/src/app/pages/skeleton-page.ts
@@ -6,7 +6,13 @@ import { ScSkeleton } from '@semantic-components/ui';
   selector: 'app-skeleton-page',
   imports: [ScSkeleton],
   template: `
-    <p>skeleton-page works!</p>
+    <div class="flex items-center space-x-4">
+      <div class="h-12 w-12 rounded-full" sc-skeleton></div>
+      <div class="space-y-2">
+        <div class="h-4 w-[250px]" sc-skeleton></div>
+        <div class="h-4 w-[200px]" sc-skeleton></div>
+      </div>
+    </div>
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
