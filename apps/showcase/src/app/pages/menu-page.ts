eef63018980673ee67da39b8b63092d6288620a6
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-19 23:14:14 +0100

    build: work in progress

--- apps/showcase/src/app/pages/menu-page.ts
+++ apps/showcase/src/app/pages/menu-page.ts
@@ -2,6 +2,7 @@ import { CdkMenuModule } from '@angular/cdk/menu';
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
 import {
+  ScButton,
   ScMenu,
   ScMenuCheckboxItem,
   ScMenuGroup,
@@ -60,92 +61,97 @@ import {
     CdkMenuModule,
     ScMenu,
     SvgChevronRightIcon,
+    ScButton,
   ],
   template: `
     <div class="m-10">
-      <div sc-menu>
-        <sc-menu-label>My Account</sc-menu-label>
-        <hr sc-menu-separator />
+      <button [scMenuTriggerFor]="menu" sc-button variant="outline">Open</button>
 
-        <sc-menu-group>
-          <button sc-menu-item>
-            <svg-user-icon />
-            <span>Profile</span>
-            <span sc-menu-shortcut>⇧⌘P</span>
-          </button>
-          <button sc-menu-item>
-            <svg-credit-card-icon />
-            <span>Billing</span>
-            <span sc-menu-shortcut>⌘B</span>
-          </button>
-          <button sc-menu-item>
-            <svg-settings-icon />
-            <span>Settings</span>
-            <span sc-menu-shortcut>⌘S</span>
-          </button>
-          <button sc-menu-item>
-            <svg-keyboard-icon />
-            <span>Keyboard shortcuts</span>
-            <span sc-menu-shortcut>⌘K</span>
-          </button>
-        </sc-menu-group>
-        <hr sc-menu-separator />
-        <sc-menu-group>
-          <button sc-menu-item>
-            <svg-users-icon />
-            <span>Team</span>
-          </button>
+      <ng-template #menu>
+        <div sc-menu>
+          <sc-menu-label>My Account</sc-menu-label>
+          <hr sc-menu-separator />
 
-          <button [scMenuTriggerFor]="sub_menu_1" sc-menu-item>
-            <svg-user-plus-icon />
-            <span>Invite users</span>
-            <svg-chevron-right-icon class="ml-auto" />
-          </button>
+          <sc-menu-group>
+            <button sc-menu-item>
+              <svg-user-icon />
+              <span>Profile</span>
+              <span sc-menu-shortcut>⇧⌘P</span>
+            </button>
+            <button sc-menu-item>
+              <svg-credit-card-icon />
+              <span>Billing</span>
+              <span sc-menu-shortcut>⌘B</span>
+            </button>
+            <button sc-menu-item>
+              <svg-settings-icon />
+              <span>Settings</span>
+              <span sc-menu-shortcut>⌘S</span>
+            </button>
+            <button sc-menu-item>
+              <svg-keyboard-icon />
+              <span>Keyboard shortcuts</span>
+              <span sc-menu-shortcut>⌘K</span>
+            </button>
+          </sc-menu-group>
+          <hr sc-menu-separator />
+          <sc-menu-group>
+            <button sc-menu-item>
+              <svg-users-icon />
+              <span>Team</span>
+            </button>
 
-          <ng-template #sub_menu_1>
-            <div sc-menu>
-              <button sc-menu-item>
-                <svg-mail-icon />
-                <span>Email</span>
-              </button>
-              <button sc-menu-item>
-                <svg-message-square-icon />
-                <span>Message</span>
-              </button>
-              <hr sc-menu-separator />
-              <button sc-menu-item>
-                <svg-circle-plus-icon />
-                <span>More...</span>
-              </button>
-            </div>
-          </ng-template>
+            <button [scMenuTriggerFor]="sub_menu_1" sc-menu-item>
+              <svg-user-plus-icon />
+              <span>Invite users</span>
+              <svg-chevron-right-icon class="ml-auto" />
+            </button>
 
+            <ng-template #sub_menu_1>
+              <div sc-menu>
+                <button sc-menu-item>
+                  <svg-mail-icon />
+                  <span>Email</span>
+                </button>
+                <button sc-menu-item>
+                  <svg-message-square-icon />
+                  <span>Message</span>
+                </button>
+                <hr sc-menu-separator />
+                <button sc-menu-item>
+                  <svg-circle-plus-icon />
+                  <span>More...</span>
+                </button>
+              </div>
+            </ng-template>
+
+            <button sc-menu-item>
+              <svg-plus-icon />
+              <span>New Team</span>
+              <span sc-menu-shortcut>⌘+T</span>
+            </button>
+          </sc-menu-group>
+          <hr sc-menu-separator />
           <button sc-menu-item>
-            <svg-plus-icon />
-            <span>New Team</span>
-            <span sc-menu-shortcut>⌘+T</span>
+            <svg-github-icon />
+            <span>GitHub</span>
           </button>
-        </sc-menu-group>
-        <hr sc-menu-separator />
-        <button sc-menu-item>
-          <svg-github-icon />
-          <span>GitHub</span>
-        </button>
-        <button sc-menu-item>
-          <svg-life-buoy-icon />
-          <span>Support</span>
-        </button>
-        <button sc-menu-item disabled>
-          <svg-cloud-icon />
-          <span>API</span>
-        </button>
-        <hr sc-menu-separator />
-        <button sc-menu-item>
-          <svg-log-out-icon />
-          <span>Log out</span>
-          <span sc-menu-shortcut>⇧⌘Q</span>
-        </button>
-      </div>
+          <button sc-menu-item>
+            <svg-life-buoy-icon />
+            <span>Support</span>
+          </button>
+          <button sc-menu-item disabled>
+            <svg-cloud-icon />
+            <span>API</span>
+          </button>
+          <hr sc-menu-separator />
+          <button sc-menu-item>
+            <svg-log-out-icon />
+            <span>Log out</span>
+            <span sc-menu-shortcut>⇧⌘Q</span>
+          </button>
+        </div>
+      </ng-template>
     </div>
 
     <br />
@@ -155,13 +161,17 @@ import {
 
     Checkboxes
 
-    <div sc-menu>
-      <sc-menu-label>Appearance</sc-menu-label>
-      <hr sc-menu-separator />
-      <button sc-menu-checkbox-item checked>Status Bar</button>
-      <button sc-menu-checkbox-item checked disabled>Activity Bar</button>
-      <button sc-menu-checkbox-item checked>Panel</button>
-    </div>
+    <button [scMenuTriggerFor]="menu2" sc-button variant="outline">Open</button>
+
+    <ng-template #menu2>
+      <div sc-menu>
+        <sc-menu-label>Appearance</sc-menu-label>
+        <hr sc-menu-separator />
+        <button sc-menu-checkbox-item checked>Status Bar</button>
+        <button sc-menu-checkbox-item checked disabled>Activity Bar</button>
+        <button sc-menu-checkbox-item checked>Panel</button>
+      </div>
+    </ng-template>
 
     <br />
     <br />
@@ -169,15 +179,19 @@ import {
 
     Radio Group
 
-    <div sc-menu>
-      <sc-menu-label>Panel Position</sc-menu-label>
-      <hr sc-menu-separator />
-      <div sc-menu-radio-group value="position">
-        <button sc-menu-radio-item value="top">Top</button>
-        <button sc-menu-radio-item value="bottom">Bottom</button>
-        <button sc-menu-radio-item value="right">Right</button>
+    <button [scMenuTriggerFor]="menu3" sc-button variant="outline">Open</button>
+
+    <ng-template #menu3>
+      <div sc-menu>
+        <sc-menu-label>Panel Position</sc-menu-label>
+        <hr sc-menu-separator />
+        <div sc-menu-radio-group value="position">
+          <button sc-menu-radio-item value="top">Top</button>
+          <button sc-menu-radio-item value="bottom">Bottom</button>
+          <button sc-menu-radio-item value="right">Right</button>
+        </div>
       </div>
-    </div>
+    </ng-template>
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
