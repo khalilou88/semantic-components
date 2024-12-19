9a434afce7718e207b19c3e43c3d0aa689119d39
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-20 00:37:07 +0100

    build: work in progress

--- libs/ngx-schematics/src/lib/ngx-schematics/ngx-schematics.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-ngx-schematics',
-  imports: [],
-  template: '',
-  styles: '',
-})
-export class NgxSchematicsComponent {}
