f7a950ae0e4cc7f00b5ea894d3c020ecd75a0b1f
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-19 22:07:18 +0100

    build: work in progress

--- apps/showcase/src/app/pages/menu-bar-page.ts
+++ apps/showcase/src/app/pages/menu-bar-page.ts
@@ -101,6 +101,123 @@ import { SvgChevronRightIcon } from '@semantic-icons/lucide-icons';
           <button sc-menu-item>Plain text</button>
         </div>
       </ng-template>
+
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <div sc-menu-bar>
+        <button [scMenuTriggerFor]="file2" sc-menu-bar-item>File</button>
+
+        <ng-template #file2>
+          <div sc-menu>
+            <button sc-menu-item>
+              New Tab
+              <span sc-menu-shortcut>⌘T</span>
+            </button>
+            <button sc-menu-item>
+              New Window
+              <span sc-menu-shortcut>⌘N</span>
+            </button>
+            <button sc-menu-item disabled>New Incognito Window</button>
+            <hr sc-menu-separator />
+            <button sc-menu-item>
+              <button [scMenuTriggerFor]="share" sc-menu-item>
+                Share
+                <svg-chevron-right-icon class="ml-auto" />
+              </button>
+              <ng-template #share>
+                <div sc-menu>
+                  <button sc-menu-item>Email link</button>
+                  <button sc-menu-item>Messages</button>
+                  <button sc-menu-item>Notes</button>
+                </div>
+              </ng-template>
+            </button>
+
+            <hr sc-menu-separator />
+            <button sc-menu-item>
+              Print...
+              <span sc-menu-shortcut>⌘P</span>
+            </button>
+          </div>
+        </ng-template>
+
+        <button [scMenuTriggerFor]="edit2" sc-menu-bar-item>Edit</button>
+        <ng-template #edit2>
+          <div sc-menu>
+            <button sc-menu-item>
+              Undo
+              <span sc-menu-shortcut>⌘Z</span>
+            </button>
+            <button sc-menu-item>
+              Redo
+              <span sc-menu-shortcut>⇧⌘Z</span>
+            </button>
+            <hr sc-menu-separator />
+            <button sc-menu-item>
+              <button [scMenuTriggerFor]="find" sc-menu-item>
+                Find
+                <svg-chevron-right-icon class="ml-auto" />
+              </button>
+              <ng-template #find>
+                <div sc-menu>
+                  <button sc-menu-item>Search the web</button>
+                  <hr sc-menu-separator />
+                  <button sc-menu-item>Find...</button>
+                  <button sc-menu-item>Find Next</button>
+                  <button sc-menu-item>Find Previous</button>
+                </div>
+              </ng-template>
+            </button>
+            <hr sc-menu-separator />
+            <button sc-menu-item>Cut</button>
+            <button sc-menu-item>Copy</button>
+            <button sc-menu-item>Paste</button>
+          </div>
+        </ng-template>
+
+        <button [scMenuTriggerFor]="view" sc-menu-bar-item>View</button>
+        <ng-template #view>
+          <div sc-menu>
+            <div sc-menu-checkbox-group>
+              <button sc-menu-checkbox-item>Always Show Bookmarks Bar</button>
+              <button sc-menu-checkbox-item checked>Always Show Full URLs</button>
+            </div>
+
+            <hr sc-menu-separator />
+            <button sc-menu-item inset>
+              Reload
+              <span sc-menu-shortcut>⌘R</span>
+            </button>
+            <button sc-menu-item disabled inset>
+              Force Reload
+              <span sc-menu-shortcut>⇧⌘R</span>
+            </button>
+            <hr sc-menu-separator />
+            <button sc-menu-item inset>Toggle Fullscreen</button>
+            <hr sc-menu-separator />
+            <button sc-menu-item inset>Hide Sidebar</button>
+          </div>
+        </ng-template>
+
+        <button [scMenuTriggerFor]="profiles" sc-menu-bar-item>Profiles</button>
+        <ng-template #profiles>
+          <div sc-menu>
+            <div sc-menu-radio-group value="benoit">
+              <button sc-menu-radio-item value="andy">Andy</button>
+              <button sc-menu-radio-item value="benoit">Benoit</button>
+              <button sc-menu-radio-item value="Luis">Luis</button>
+            </div>
+
+            <hr sc-menu-separator />
+            <button sc-menu-item inset>Edit...</button>
+            <hr sc-menu-separator />
+            <button sc-menu-item inset>Add Profile...</button>
+          </div>
+        </ng-template>
+      </div>
     </div>
   `,
   styles: ``,
