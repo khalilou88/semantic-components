2bb6703082ec4fa45019fd4fb52ea7d50bf92bfb
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-22 14:49:42 +0100

    build: work in progress

--- libs/ui/src/lib/components/sidebar/sidebar-provider.ts
+++ libs/ui/src/lib/components/sidebar/sidebar-provider.ts
@@ -3,12 +3,18 @@ import {
   Component,
   ViewEncapsulation,
   computed,
+  inject,
   input,
   signal,
 } from '@angular/core';
 
 import { cn } from '../../utils';
 import { SIDEBAR_WIDTH, SIDEBAR_WIDTH_ICON } from './constants';
+import { ScSidebarState } from './sidebar-state';
+
+export const scPanelClasses = signal(
+  'bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
+);
 
 @Component({
   selector: 'sc-sidebar-provider',
@@ -19,6 +25,7 @@ import { SIDEBAR_WIDTH, SIDEBAR_WIDTH_ICON } from './constants';
   host: {
     '[class]': 'classes()',
     '[style]': 'styles()',
+    '[attr.data-state]': 'stateMobile()',
   },
   styles: ``,
   encapsulation: ViewEncapsulation.None,
@@ -26,11 +33,14 @@ import { SIDEBAR_WIDTH, SIDEBAR_WIDTH_ICON } from './constants';
   providers: [],
 })
 export class ScSidebarProvider {
+  sidebarState = inject(ScSidebarState);
+
   class = input<string>('');
 
   classes = computed(() =>
     cn(
       'group/sidebar-wrapper flex min-h-svh w-full has-[[data-variant=inset]]:bg-sidebar',
+      this.sidebarState.openMobile() && scPanelClasses(),
       this.class(),
     ),
   );
@@ -38,4 +48,8 @@ export class ScSidebarProvider {
   styles = signal(
     `--sidebar-width: ${SIDEBAR_WIDTH}; --sidebar-width-icon: ${SIDEBAR_WIDTH_ICON};`,
   );
+
+  stateMobile = computed<'open' | 'closed'>(() => {
+    return this.sidebarState.openMobile() ? 'open' : 'closed';
+  });
 }
