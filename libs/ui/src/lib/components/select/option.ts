f9ea5c97f15b57b97d63cd23389e8f1302f33ed1
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-23 00:46:56 +0100

    build: work in progress

--- libs/ui/src/lib/components/select/option.ts
+++ libs/ui/src/lib/components/select/option.ts
@@ -11,34 +11,42 @@ import {
 
 import { SvgCheckIcon } from '@semantic-icons/lucide-icons';
 
+import { cn } from '../../utils';
 import { ScOptionModel } from './option-model';
 import { ScSelectState } from './select-state';
 
 @Component({
-  selector: 'sc-option',
+  selector: 'button[sc-option]',
   imports: [SvgCheckIcon],
   template: `
-    <button
-      class="relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50"
-      (click)="select()"
-      type="button"
-    >
-      @if (isSelected()) {
-        <span class="absolute left-2 flex size-3.5 items-center justify-center">
-          <svg-check-icon class="size-4" />
-        </span>
-      }
+    @if (isSelected()) {
+      <span class="absolute left-2 flex size-3.5 items-center justify-center">
+        <svg-check-icon class="size-4" />
+      </span>
+    }
 
-      {{ _isActive() }}
+    {{ _isActive() }}
 
-      <ng-content />
-    </button>
+    <ng-content />
   `,
+  host: {
+    '[class]': 'classes()',
+    '(click)': 'select()',
+  },
   styles: ``,
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ScOption implements Highlightable {
+  class = input<string>('');
+
+  classes = computed(() =>
+    cn(
+      'relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
+      this.class(),
+    ),
+  );
+
   state = inject(ScSelectState);
 
   option = input.required<ScOptionModel>();
--- libs/ui/src/lib/components/select/select2.ts
+++ libs/ui/src/lib/components/select/select2.ts
@@ -18,17 +18,16 @@ import { ScSelectState } from './select-state';
   selector: 'sc-select2',
   imports: [ScOption, A11yModule],
   template: `
-    <button
+    <div
       class="relative z-50 max-h-96 w-full min-w-32 overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"
       [cdkTrapFocusAutoCapture]="true"
       [id]="_getPanelId()"
-      (keyup)="onKeydown($event)"
       cdkTrapFocus
     >
       @for (option of options(); track option.value) {
-        <sc-option [option]="option">{{ option.label }}</sc-option>
+        <button [option]="option" (keyup)="onKeydown($event)" sc-option>{{ option.label }}</button>
       }
-    </button>
+    </div>
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
