2d85cc49ae8780623b8f30ede5899a353e280025
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-23 00:27:56 +0100

    build: work in progress

--- libs/ui/src/lib/components/select/select2.ts
+++ libs/ui/src/lib/components/select/select2.ts
@@ -18,7 +18,7 @@ import { ScSelectState } from './select-state';
   selector: 'sc-select2',
   imports: [ScOption, A11yModule],
   template: `
-    <div
+    <button
       class="relative z-50 max-h-96 w-full min-w-32 overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"
       [cdkTrapFocusAutoCapture]="true"
       [id]="_getPanelId()"
@@ -28,7 +28,7 @@ import { ScSelectState } from './select-state';
       @for (option of options(); track option.value) {
         <sc-option [option]="option">{{ option.label }}</sc-option>
       }
-    </div>
+    </button>
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
