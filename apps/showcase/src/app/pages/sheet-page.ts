53a8027d4fcfad31839019fa30cd9deb732ad195
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-22 15:36:48 +0100

    build: work in progress

--- apps/showcase/src/app/pages/sheet-page.ts
+++ apps/showcase/src/app/pages/sheet-page.ts
@@ -33,26 +33,24 @@ import {
     ScCardDescription,
   ],
   template: `
-    <ng-template>
+    <ng-template #sheet>
       <div sc-sheet>
         <div sc-card-header>
           <h2 sc-card-title>Edit profile</h2>
-          <p sc-card-description>
-            Make changes to your profile here. Click save when you're done.
-          </p>
-        <div>
+          <p sc-card-description>Make changes to your profile here. Click save when you're done.</p>
+        </div>
         <div class="grid gap-4 py-4">
           <div class="grid grid-cols-4 items-center gap-4">
-            <label sc-label for="name" class="text-right">Name</label>
-            <input sc-input id="name" value="Pedro Duarte" class="col-span-3" />
+            <label class="text-right" sc-label for="name">Name</label>
+            <input class="col-span-3" id="name" sc-input value="Pedro Duarte" />
           </div>
           <div class="grid grid-cols-4 items-center gap-4">
-            <label sc-label for="username" class="text-right">Username</label>
-            <input sc-input id="username" value="@peduarte" class="col-span-3" />
+            <label class="text-right" sc-label for="username">Username</label>
+            <input class="col-span-3" id="username" sc-input value="@peduarte" />
           </div>
         </div>
         <div sc-card-footer>
-            <button sc-button type="submit">Save changes</button>
+          <button sc-button type="submit">Save changes</button>
         </div>
       </div>
     </ng-template>
