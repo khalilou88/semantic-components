59e9f73dc9dcdf50675a00fbe55283f1f9eba053
Khalil LAGRIDA <32600911+khalilou88@users.noreply.github.com>
2024-12-23 13:39:07 +0100

    Option (#67)
    * build: work in progress
    
    * build: work in progress
    
    * build: work in progress
    
    * build: work in progress

--- libs/ui/src/lib/components/select/select-listbox.ts
+++ libs/ui/src/lib/components/select/select-listbox.ts
@@ -1,13 +1,12 @@
-import { A11yModule, ActiveDescendantKeyManager } from '@angular/cdk/a11y';
-import { ENTER } from '@angular/cdk/keycodes';
+import { A11yModule } from '@angular/cdk/a11y';
+import { CdkListbox, CdkOption } from '@angular/cdk/listbox';
 import {
-  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
   ViewEncapsulation,
   inject,
   input,
-  viewChildren,
+  output,
 } from '@angular/core';
 
 import { ScOption } from './option';
@@ -15,52 +14,34 @@ import { ScOptionModel } from './option-model';
 import { ScSelectState } from './select-state';
 
 @Component({
-  selector: 'sc-select2',
-  imports: [ScOption, A11yModule],
+  selector: 'sc-select-listbox',
+  imports: [ScOption, A11yModule, CdkListbox, CdkOption],
   template: `
-    <button
+    <div
       class="relative z-50 max-h-96 w-full min-w-32 overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"
       [cdkTrapFocusAutoCapture]="true"
-      [id]="_getPanelId()"
-      (keyup)="onKeydown($event)"
+      (cdkListboxValueChange)="handleOptionChange($event.value)"
       cdkTrapFocus
+      cdkListbox
     >
       @for (option of options(); track option.value) {
-        <sc-option [option]="option">{{ option.label }}</sc-option>
+        <button [cdkOption]="option" [option]="option" sc-option>{{ option.label }}</button>
       }
-    </button>
+    </div>
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ScSelect2 implements AfterViewInit {
-  //TODO change this
-  _getPanelId() {
-    return `panel-12584`;
-  }
-
+export class ScSelectListbox {
   state = inject(ScSelectState);
 
   options = input<ScOptionModel[]>([]);
 
-  readonly viewOptions = viewChildren(ScOption);
-
-  private keyManager!: ActiveDescendantKeyManager<ScOption>;
-
-  ngAfterViewInit(): void {
-    console.log(this.viewOptions());
-
-    this.keyManager = new ActiveDescendantKeyManager(this.viewOptions()).withWrap();
+  handleOptionChange(v: readonly unknown[]) {
+    this.state.selectedOption.set(v[0] as ScOptionModel);
+    this.optionSelected.emit();
   }
 
-  onKeydown(event: KeyboardEvent) {
-    if (event.keyCode === ENTER) {
-      this.state.selectedOption.set(
-        this.keyManager.activeItem ? this.keyManager.activeItem.option() : undefined,
-      );
-    } else {
-      this.keyManager.onKeydown(event);
-    }
-  }
+  optionSelected = output();
 }
