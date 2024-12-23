b7b53db0b3f3baba34f29d9a69942e4012b30b28
Khalil LAGRIDA <32600911+khalilou88@users.noreply.github.com>
2024-12-23 12:22:51 +0100

    Work in progress sheet (#66)
    * build: work in progress
    
    * build: work in progress
    
    * build: work in progress

--- libs/ui/src/lib/components/sheet/sheet-trigger.ts
+++ libs/ui/src/lib/components/sheet/sheet-trigger.ts
@@ -1,15 +1,8 @@
-import {
-  ConnectedPosition,
-  Overlay,
-  OverlayContainer,
-  OverlayRef,
-  PositionStrategy,
-} from '@angular/cdk/overlay';
+import { Overlay, OverlayConfig, OverlayContainer, OverlayRef } from '@angular/cdk/overlay';
 import { ComponentPortal } from '@angular/cdk/portal';
 import { ComponentRef, Injectable, TemplateRef, inject, signal } from '@angular/core';
 
 import { scOverlayClasses } from '../overlay';
-import { SheetVariants } from './sheet';
 import { ScSheetConfig } from './sheet-config';
 import { ScSheetContainer } from './sheet-container';
 
@@ -17,10 +10,9 @@ import { ScSheetContainer } from './sheet-container';
   providedIn: 'root',
 })
 export class ScSheetTrigger {
-  private _overlayContainer = inject(OverlayContainer);
-
-  private readonly overlay = inject(Overlay);
-  private overlayRef!: OverlayRef;
+  private readonly _overlayContainer = inject(OverlayContainer);
+  private readonly _overlay = inject(Overlay);
+  private _overlayRef!: OverlayRef;
 
   state = signal<'open' | 'closed'>('closed');
   side = signal<'top' | 'bottom' | 'left' | 'right'>('right');
@@ -31,75 +23,59 @@ export class ScSheetTrigger {
     this._overlayContainer.getContainerElement().classList.add(...scOverlayClasses());
     this._overlayContainer.getContainerElement().setAttribute('data-state', 'open');
 
-    const positionStrategy = this.getPositionStrategy(config.side);
-    this.overlayRef = this.overlay.create({ positionStrategy });
-
-    this.overlayRef.updateSize({ height: config.height, width: config.width });
+    const overlayConfig = this.getOverlayConfig(config);
+    this._overlayRef = this._overlay.create(overlayConfig);
 
     const scSheetPortal = new ComponentPortal(ScSheetContainer);
-
-    const scSheetRef: ComponentRef<ScSheetContainer> = this.overlayRef.attach(scSheetPortal);
+    const scSheetRef: ComponentRef<ScSheetContainer> = this._overlayRef.attach(scSheetPortal);
 
     scSheetRef.instance.templateRef.set(templateRef);
+
+    this._overlayRef.backdropClick().subscribe(() => this.close());
   }
 
   close() {
-    if (this.overlayRef?.hasAttached() === true) {
-      this.overlayRef?.detach();
+    if (this._overlayRef?.hasAttached()) {
+      this._overlayRef?.detach();
       this.state.set('closed');
       this._overlayContainer.getContainerElement().classList.remove(...scOverlayClasses());
       this._overlayContainer.getContainerElement().setAttribute('data-state', 'closed');
     }
   }
 
-  private getPositionStrategy(side: SheetVariants['side']): PositionStrategy {
-    return this.overlay
-      .position()
-      .flexibleConnectedTo(this._overlayContainer.getContainerElement())
-      .withPositions(this.getPositions(side));
-  }
-
-  getPositions(side: SheetVariants['side']): ConnectedPosition[] {
-    switch (side) {
+  private getOverlayConfig(config: ScSheetConfig): OverlayConfig {
+    switch (config.side) {
       case 'top': {
-        return [
-          {
-            originX: 'start',
-            originY: 'top',
-            overlayX: 'start',
-            overlayY: 'top',
-          },
-        ];
+        return new OverlayConfig({
+          positionStrategy: this._overlay.position().global().top(),
+          width: config.width,
+          height: config.height,
+          hasBackdrop: true,
+        });
       }
       case 'bottom': {
-        return [
-          {
-            originX: 'end',
-            originY: 'bottom',
-            overlayX: 'end',
-            overlayY: 'bottom',
-          },
-        ];
+        return new OverlayConfig({
+          positionStrategy: this._overlay.position().global().bottom(),
+          width: config.width,
+          height: config.height,
+          hasBackdrop: true,
+        });
       }
       case 'left': {
-        return [
-          {
-            originX: 'start',
-            originY: 'top',
-            overlayX: 'start',
-            overlayY: 'top',
-          },
-        ];
+        return new OverlayConfig({
+          positionStrategy: this._overlay.position().global().left(),
+          width: config.width,
+          height: config.height,
+          hasBackdrop: true,
+        });
       }
       case 'right': {
-        return [
-          {
-            originX: 'end',
-            originY: 'top',
-            overlayX: 'end',
-            overlayY: 'top',
-          },
-        ];
+        return new OverlayConfig({
+          positionStrategy: this._overlay.position().global().right(),
+          width: config.width,
+          height: config.height,
+          hasBackdrop: true,
+        });
       }
 
       default: {
