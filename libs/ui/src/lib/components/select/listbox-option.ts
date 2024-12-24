759ea57864bc45c1f85be0a84292902d63471fdc
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-24 19:40:05 +0100

    build: work in progress

--- libs/ui/src/lib/components/select/listbox-option.ts
+++ libs/ui/src/lib/components/select/listbox-option.ts
@@ -32,7 +32,7 @@ export class ScListboxOption {
 
   classes = computed(() =>
     cn(
-      'relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
+      'relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50',
       this.class(),
     ),
   );
