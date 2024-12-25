65c701ce1b6c73f603838ceaf83ba5ae71d103cc
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-12-25 23:14:50 +0100

    build: work in progress

--- libs/ui/src/lib/components/file-upload/simple-dnd-upload-files.ts
+++ libs/ui/src/lib/components/file-upload/simple-dnd-upload-files.ts
@@ -22,52 +22,54 @@ const ALLOWED_FILE_TYPES = ['image/jpeg', 'image/png', 'image/svg+xml'];
       <h1 class="leading-none text-4xl font-bold mb-[50px]">Drag and Drop Files Upload</h1>
 
       <div class="flex items-center content-center min-h-[350px] gap-x-6">
-        <!-- Start Drop Zone -->
-        <div
-          class="relative flex flex-col outline-gray-500 outline-2 -outline-offset-4 outline-dashed w-[350px] h-[350px] rounded-2xl items-center justify-center"
-        >
-          <input
-            class="absolute w-full h-full cursor-pointer opacity-0 top-0 left-0"
-            #fileInput
-            [accept]="allowedFileTypes"
-            (change)="handleChange($event)"
-            type="file"
-          />
-
-          <span class="mb-6">
-            <svg
-              class="w-10 h-10"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15m0-3-3-3m0 0-3 3m3-3V15"
-              />
-            </svg>
-          </span>
-
-          <p class="text-center text-base max-w-[90%]">
-            Drag and drop
-            <br />
-            file here to upload.
-            <br />
-            (PNG, JPG, SVG)
-          </p>
-
-          <button
-            class="z-10 mt-6 text-green-50 px-6 py-3 bg-green-500 rounded-lg font-medium text-base hover:bg-green-600 ease-in-out"
-            (click)="fileInput.click()"
-            type="button"
+        @if (!fileUrl()) {
+          <!-- Start Drop Zone -->
+          <div
+            class="relative flex flex-col outline-gray-500 outline-2 -outline-offset-4 outline-dashed w-[350px] h-[350px] rounded-2xl items-center justify-center"
           >
-            Browse File
-          </button>
-        </div>
-        <!-- End Drop Zone -->
+            <input
+              class="absolute w-full h-full cursor-pointer opacity-0 top-0 left-0"
+              #fileInput
+              [accept]="allowedFileTypes"
+              (change)="handleChange($event)"
+              type="file"
+            />
+
+            <span class="mb-6">
+              <svg
+                class="w-10 h-10"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15m0-3-3-3m0 0-3 3m3-3V15"
+                />
+              </svg>
+            </span>
+
+            <p class="text-center text-base max-w-[90%]">
+              Drag and drop
+              <br />
+              file here to upload.
+              <br />
+              (PNG, JPG, SVG)
+            </p>
+
+            <button
+              class="z-10 mt-6 text-green-50 px-6 py-3 bg-green-500 rounded-lg font-medium text-base hover:bg-green-600 ease-in-out"
+              (click)="fileInput.click()"
+              type="button"
+            >
+              Browse File
+            </button>
+          </div>
+          <!-- End Drop Zone -->
+        }
 
         <!-- Start Preview Image -->
         @if (fileUrl()) {
@@ -79,17 +81,6 @@ const ALLOWED_FILE_TYPES = ['image/jpeg', 'image/png', 'image/svg+xml'];
               <p class="mb-6">{{ file()?.name }}</p>
 
               <div class="flex gap-3 justify-start">
-                @if (status() === 'success') {
-                  <button
-                    class="text-sm font-medium w-[50%] border border-blue-500 px-4 py-3 bg-blue-500 text-blue-50 rounded-lg hover:bg-blue-600 ease-in-out disabled:bg-gray-300 disabled:border-gray-300"
-                    [disabled]="status() === 'uploading'"
-                    (click)="uploadFile()"
-                    type="button"
-                  >
-                    {{ status() === 'uploading' ? 'UPLOADING...' : 'UPLOAD' }}
-                  </button>
-                }
-
                 <button
                   class="text-sm font-medium w-[50%] border border-red-500 px-4 py-3 rounded-lg text-red-500 hover:bg-red-500 hover:text-white ease-in-out"
                   [disabled]="status() === 'uploading'"
@@ -98,6 +89,17 @@ const ALLOWED_FILE_TYPES = ['image/jpeg', 'image/png', 'image/svg+xml'];
                 >
                   REMOVE
                 </button>
+
+                @if (status() === 'init' || status() === 'error') {
+                  <button
+                    class="text-sm font-medium w-[50%] border border-blue-500 px-4 py-3 bg-blue-500 text-blue-50 rounded-lg hover:bg-blue-600 ease-in-out disabled:bg-gray-300 disabled:border-gray-300"
+                    [disabled]="status() === 'uploading'"
+                    (click)="uploadFile()"
+                    type="button"
+                  >
+                    Upload
+                  </button>
+                }
 
                 @if (status() === 'uploading') {
                   <button
@@ -152,8 +154,8 @@ export class ScSimpleDndUploadFiles {
   }
 
   removeFile() {
-    this.fileInput().nativeElement.value = '';
     this.file.set(null);
+    this.status.set('init');
   }
 
   dataService = inject(DataService);
