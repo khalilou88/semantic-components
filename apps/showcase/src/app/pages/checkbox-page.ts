f3ef72c961f1482ad31a9c059984db5950048cd2
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-01-09 00:46:27 +0100

    build: work in progress

--- apps/showcase/src/app/pages/checkbox-page.ts
+++ apps/showcase/src/app/pages/checkbox-page.ts
@@ -8,6 +8,14 @@ import { ScCheckbox } from '@semantic-components/ui';
   selector: 'app-checkbox-page',
   imports: [ScCheckbox, ReactiveFormsModule, JsonPipe],
   template: `
+    <div class="flex items-center space-x-2">
+      <sc-checkbox>Accept terms and conditions</sc-checkbox>
+    </div>
+
+    <br />
+    <br />
+    <br />
+
     <form [formGroup]="checkForm">
       <sc-checkbox formControlName="check" />
     </form>
