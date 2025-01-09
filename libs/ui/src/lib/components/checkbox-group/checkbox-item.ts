ddb9147c97b37497e0670ca8a583e1e643b2f433
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-01-09 11:13:37 +0100

    build: work in progress

--- libs/ui/src/lib/components/checkbox-group/checkbox-item.ts
+++ libs/ui/src/lib/components/checkbox-group/checkbox-item.ts
@@ -30,9 +30,9 @@ import { SiCheckIcon } from '@semantic-icons/lucide-icons';
         class="peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground"
         [id]="id()"
         [attr.data-state]="state()"
+        [attr.aria-checked]="state() === 'checked' ? 'true' : 'false'"
         type="button"
         role="checkbox"
-        aria-checked="true"
         value="on"
         aria-describedby=":rce:-form-item-description"
         aria-invalid="false"
@@ -45,11 +45,11 @@ import { SiCheckIcon } from '@semantic-icons/lucide-icons';
         </span>
       </button>
       <input
+        [checked]="checked()"
         aria-hidden="true"
         tabindex="-1"
         type="checkbox"
         value="on"
-        checked=""
         style="transform: translateX(-100%); position: absolute; pointer-events: none; opacity: 0; margin: 0px; width: 16px; height: 16px;"
       />
     </div>
