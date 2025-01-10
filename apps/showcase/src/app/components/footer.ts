b61d37d2e3cdad0eb60d91863c477a6e88af73a0
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-01-10 16:58:46 +0100

    build: work in progress

--- apps/showcase/src/app/components/footer.ts
+++ apps/showcase/src/app/components/footer.ts
@@ -9,19 +9,19 @@ import { SiGithubIcon } from '@semantic-icons/lucide-icons';
     <footer class="bg-white dark:bg-gray-900">
       <div class="mx-auto w-full">
         <div
-          class="px-4 py-6 bg-gray-100 dark:bg-gray-700 md:flex md:items-center md:justify-between"
+          class="bg-gray-100 px-4 py-6 md:flex md:items-center md:justify-between dark:bg-gray-700"
         >
-          <span class="text-sm text-gray-500 dark:text-gray-300 sm:text-center">
+          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-300">
             © 2025
             <a href="">Semantic Components</a>
             . All Rights Reserved.
           </span>
-          <div class="flex mt-4 sm:justify-center md:mt-0 space-x-5 rtl:space-x-reverse">
+          <div class="mt-4 flex space-x-5 sm:justify-center md:mt-0 rtl:space-x-reverse">
             <a
               class="text-gray-400 hover:text-gray-900 dark:hover:text-white"
               href="https://github.com/khalilou88/semantic-components"
             >
-              <svg class="w-4 h-4" aria-hidden="true" si-github-icon></svg>
+              <svg class="size-4" aria-hidden="true" si-github-icon></svg>
               <span class="sr-only">Github</span>
             </a>
           </div>
