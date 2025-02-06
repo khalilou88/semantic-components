3ff00c3ee9edcd0ff33bd9d604fbc07455cd0beb
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-02-06 18:33:56 +0100

    build: work in progress

--- libs/ui/src/lib/components/button/button.ts
+++ libs/ui/src/lib/components/button/button.ts
@@ -1,80 +1,18 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  ViewEncapsulation,
-  booleanAttribute,
-  computed,
-  input,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
-import { cn } from '@semantic-components/utils';
-import { VariantProps, cva } from 'class-variance-authority';
-
-import { ScCursor } from './cursor';
-
-export const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
-  {
-    variants: {
-      variant: {
-        primary: 'bg-primary text-primary-foreground hover:bg-primary/90',
-        destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
-        outline: 'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
-        secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline',
-      },
-      size: {
-        default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
-        lg: 'h-11 rounded-md px-8',
-        icon: 'size-10',
-      },
-    },
-    defaultVariants: {
-      variant: 'primary',
-      size: 'default',
-    },
-  },
-);
-
-export type ButtonVariants = VariantProps<typeof buttonVariants>;
+import { ScButtonBase } from './button-base';
 
 @Component({
-  selector: 'button[sc-button], a[sc-button]',
+  selector: 'button[sc-button]',
   imports: [],
   template: `
     <ng-content />
   `,
   host: {
-    '[class]': 'class()',
-    '[attr.data-disabled]': 'disabled()',
     'data-slot': 'button',
   },
   styles: ``,
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  hostDirectives: [
-    {
-      directive: ScCursor,
-      inputs: ['disabled'],
-    },
-  ],
 })
-export class ScButton {
-  readonly variant = input<ButtonVariants['variant']>('primary');
-
-  readonly size = input<ButtonVariants['size']>('default');
-
-  readonly classInput = input<string>('', {
-    alias: 'class',
-  });
-
-  protected readonly class = computed(() =>
-    cn(buttonVariants({ variant: this.variant(), size: this.size() }), this.classInput()),
-  );
-
-  readonly disabled = input<boolean, unknown>(false, {
-    transform: booleanAttribute,
-  });
-}
+export class ScButton extends ScButtonBase {}
