1106663ee31401bd82f7ed1b9408a9cb2eedb710
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-02-06 12:49:06 +0100

    build: work in progress

--- apps/showcase/src/app/pages/input-group-page.ts
+++ apps/showcase/src/app/pages/input-group-page.ts
@@ -1,14 +1,17 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
 import { ScInput, ScInputGroup } from '@semantic-components/ui';
+import { SiCircleHelpIcon, SiDollarSignIcon } from '@semantic-icons/lucide-icons';
 
 @Component({
   selector: 'app-input-group-page',
-  imports: [ScInputGroup, ScInput],
+  imports: [ScInputGroup, ScInput, SiDollarSignIcon, SiCircleHelpIcon],
   template: `
     <div class="m-10">
       <sc-input-group>
+        <svg si-dollar-sign-icon></svg>
         <input sc-input />
+        <svg si-circle-help-icon></svg>
       </sc-input-group>
     </div>
   `,
