53447f1c85a6b3afb93369e9f698654aeed1551e
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-02-10 10:06:34 +0100

    build: work in progress

--- libs/ui/src/lib/components/checkbox/checkbox-field.ts
+++ libs/ui/src/lib/components/checkbox/checkbox-field.ts
@@ -35,7 +35,25 @@ export class ScCheckboxField {
     alias: 'class',
   });
 
-  protected readonly class = computed(() => cn('', this.classInput()));
+  protected readonly class = computed(() =>
+    cn(
+      // Base layout
+      'grid grid-cols-[1.125rem_1fr] items-center gap-x-4 gap-y-1 sm:grid-cols-[1rem_1fr]',
+
+      // Control layout
+      '[&>[data-slot=control]]:col-start-1 [&>[data-slot=control]]:row-start-1 [&>[data-slot=control]]:justify-self-center',
+
+      // Label layout
+      '[&>[data-slot=label]]:col-start-2 [&>[data-slot=label]]:row-start-1 [&>[data-slot=label]]:justify-self-start',
+
+      // Description layout
+      '[&>[data-slot=description]]:col-start-2 [&>[data-slot=description]]:row-start-2',
+
+      // With description
+      '[&_[data-slot=label]]:has-[[data-slot=description]]:font-medium',
+      this.classInput(),
+    ),
+  );
 
   readonly id = signal<string>(inject(_IdGenerator).getId('sc-checkbox-field-'));
 
