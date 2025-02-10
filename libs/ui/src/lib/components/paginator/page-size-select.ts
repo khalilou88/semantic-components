9948395266e40c36967f90fe58b2bb224d0bda67
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-02-10 10:49:54 +0100

    build: work in progress

--- libs/ui/src/lib/components/paginator/page-size-select.ts
+++ libs/ui/src/lib/components/paginator/page-size-select.ts
@@ -1,13 +1,32 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation, input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+import { ScOption, ScSelect } from '../select';
+
+//TODO remove this
+/** The default page size if there is no page size and there are no provided page size options. */
+const DEFAULT_PAGE_SIZE = 10;
 
 @Component({
   selector: 'sc-page-size-select',
-  imports: [],
+  imports: [ScSelect, ScOption, ReactiveFormsModule],
   template: `
-    <p>page-size-select works!</p>
+    <sc-select class="inline-block" [formControl]="pageSizeFormControl">
+      @for (pageSizeOption of pageSizeOptions(); track $index) {
+        <sc-option [value]="pageSizeOption">{{ pageSizeOption }}</sc-option>
+      }
+    </sc-select>
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ScPageSizeSelect {}
+export class ScPageSizeSelect {
+  /** The set of provided page size options to display to the user. */
+  readonly pageSizeOptions = input<number[]>([5, DEFAULT_PAGE_SIZE, 25]);
+
+  /** Number of items to display on a page. By default, set to 10. */
+  readonly pageSize = input<number>(DEFAULT_PAGE_SIZE);
+
+  pageSizeFormControl = new FormControl(this.pageSize());
+}
