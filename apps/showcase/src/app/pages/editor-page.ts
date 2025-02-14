079d2495a1de0ef8ca4c22a68afa71c49518db2b
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-02-14 11:46:18 +0100

    build: work in progress

--- apps/showcase/src/app/pages/editor-page.ts
+++ apps/showcase/src/app/pages/editor-page.ts
@@ -1,18 +1,214 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
-import { ScEditor } from '@semantic-components/ui';
+import {
+  ScBreadcrumb,
+  ScBreadcrumbItem,
+  ScBreadcrumbLink,
+  ScBreadcrumbList,
+  ScBreadcrumbPage,
+  ScBreadcrumbSeparator,
+  ScCard,
+  ScCardContent,
+  ScCodeHighlighter,
+  ScEditor,
+  ScHeading,
+  ScPageDescription,
+  ScPageSubtitle,
+  ScPageTitle,
+  ScTab,
+  ScTabContent,
+  ScTabLabel,
+  ScTabs,
+} from '@semantic-components/ui';
+import { SiChevronRightIcon } from '@semantic-icons/lucide-icons';
 
 @Component({
   selector: 'app-editor-page',
-  imports: [ScEditor, ReactiveFormsModule],
+  imports: [
+    ScEditor,
+    ReactiveFormsModule,
+    SiChevronRightIcon,
+    ScBreadcrumb,
+    ScBreadcrumbList,
+    ScBreadcrumbItem,
+    ScBreadcrumbLink,
+    ScBreadcrumbPage,
+    ScBreadcrumbSeparator,
+    ScPageTitle,
+    ScPageSubtitle,
+    ScPageDescription,
+    ScTabs,
+    ScTab,
+    ScTabLabel,
+    ScTabContent,
+    ScCard,
+    ScHeading,
+    ScCodeHighlighter,
+    ScCardContent,
+    RouterLink,
+  ],
   template: `
-    <div class="m-10">
-      <form [formGroup]="editorForm">
-        <sc-editor formControlName="content" />
-      </form>
+    <div class="relative py-6 lg:gap-10 lg:py-8 xl:grid xl:grid-cols-[1fr_300px] px-4">
+      <div class="mx-auto w-full max-w-3xl">
+        <nav sc-breadcrumb>
+          <ol sc-breadcrumb-list>
+            <li sc-breadcrumb-item><a sc-breadcrumb-link>Components</a></li>
+
+            <li sc-breadcrumb-separator><svg si-chevron-right-icon></svg></li>
+            <li sc-breadcrumb-item>
+              <span sc-breadcrumb-page>Editor</span>
+            </li>
+          </ol>
+        </nav>
+
+        <h1 sc-page-title>Editor</h1>
+
+        <p sc-page-description>Editor.</p>
+
+        <h2 id="usage" sc-page-subtitle>Usage</h2>
+
+        <sc-code-highlighter class="mt-2" [code]="codeSnippet1" language="typescript" />
+
+        <sc-code-highlighter class="mt-2" [code]="codeSnippet2" />
+
+        <h2 class="mb-5" id="examples" sc-page-subtitle>Examples</h2>
+
+        <section class="my-10" id="small-buttons">
+          <h3 class="mb-2" sc-heading level="3">Editor</h3>
+
+          <sc-tabs class="w-[400px]" tabsHeaderClass="grid w-full grid-cols-2">
+            <sc-tab>
+              <sc-tab-label>Preview</sc-tab-label>
+              <sc-tab-content>
+                <div class="overflow-auto" sc-card>
+                  <div class="m-10 flex gap-2 p-0" sc-card-content>
+                    <form [formGroup]="editorForm">
+                      <sc-editor formControlName="content" />
+                    </form>
+                  </div>
+                </div>
+              </sc-tab-content>
+            </sc-tab>
+
+            <sc-tab>
+              <sc-tab-label>Code</sc-tab-label>
+              <sc-tab-content>
+                <div class="overflow-auto" sc-card>
+                  <div class="m-10 flex gap-2 p-0" sc-card-content>
+                    <p>Coming soon</p>
+                  </div>
+                </div>
+              </sc-tab-content>
+            </sc-tab>
+          </sc-tabs>
+        </section>
+      </div>
+
+      <div class="hidden text-sm xl:block">
+        <div class="sticky top-20 -mt-6 h-[calc(100vh-3.5rem)] pt-4">
+          <div class="no-scrollbar h-full overflow-auto pb-10">
+            <div class="space-y-2">
+              <p class="font-medium">On This Page</p>
+              <ul class="m-0 list-none">
+                <!--li class="mt-0 pt-2">
+                  <a
+                    class="inline-block font-medium text-foreground no-underline transition-colors hover:text-foreground"
+                    href="#installation"
+                  >
+                    Installation
+                  </a>
+                </li-->
+                <li class="mt-0 pt-2">
+                  <a
+                    class="inline-block text-muted-foreground no-underline transition-colors hover:text-foreground"
+                    routerLink="."
+                    fragment="usage"
+                  >
+                    Usage
+                  </a>
+                </li>
+                <li class="mt-0 pt-2">
+                  <a
+                    class="inline-block text-muted-foreground no-underline transition-colors hover:text-foreground"
+                    routerLink="."
+                    fragment="examples"
+                  >
+                    Examples
+                  </a>
+                  <ul class="m-0 list-none pl-4">
+                    <li class="mt-0 pt-2">
+                      <a
+                        class="inline-block text-muted-foreground no-underline transition-colors hover:text-foreground"
+                        routerLink="."
+                        fragment="variants"
+                      >
+                        Variants
+                      </a>
+                    </li>
+
+                    <li class="mt-0 pt-2">
+                      <a
+                        class="inline-block text-muted-foreground no-underline transition-colors hover:text-foreground"
+                        routerLink="."
+                        fragment="small-buttons"
+                      >
+                        Small buttons
+                      </a>
+                    </li>
+
+                    <li class="mt-0 pt-2">
+                      <a
+                        class="inline-block text-muted-foreground no-underline transition-colors hover:text-foreground"
+                        routerLink="."
+                        fragment="large-buttons"
+                      >
+                        Large buttons
+                      </a>
+                    </li>
+
+                    <li class="mt-0 pt-2">
+                      <a
+                        class="inline-block text-muted-foreground no-underline transition-colors hover:text-foreground"
+                        routerLink="."
+                        fragment="disabled-buttons"
+                      >
+                        Disabled buttons
+                      </a>
+                    </li>
+
+                    <li class="mt-0 pt-2">
+                      <a
+                        class="inline-block text-muted-foreground no-underline transition-colors hover:text-foreground"
+                        routerLink="."
+                        fragment="buttons-with-icons"
+                      >
+                        Buttons with icons
+                      </a>
+                    </li>
+
+                    <li class="mt-0 pt-2">
+                      <a
+                        class="inline-block text-muted-foreground no-underline transition-colors hover:text-foreground"
+                        routerLink="."
+                        fragment="button-with-loading-state"
+                      >
+                        Button with loading state
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   `,
+  host: {
+    '[class]': 'class()',
+  },
   styles: ``,
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -32,4 +228,17 @@ export default class EditorPage {
   </p>
   `),
   });
+
+  class = signal<string>('block w-full');
+
+  codeSnippet1 = `import { ScButton } from '@semantic-components/ui';`;
+
+  codeSnippet2 = `<button sc-button type="button">Primary</button>`;
+
+  codeSnippet = `<button sc-button type="button">Primary</button>
+    <button sc-button variant="secondary" type="button">Secondary</button>
+    <button sc-button variant="destructive" type="button">Destructive</button>
+    <button sc-button variant="outline" type="button">Outline</button>
+    <button sc-button variant="ghost" type="button">Ghost</button>
+    <button sc-button variant="link" type="button">Link</button>`;
 }
