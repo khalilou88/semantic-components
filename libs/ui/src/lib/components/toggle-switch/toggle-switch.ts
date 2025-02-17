c51d25818d718e9c55437fbfb83d523e3beeb269
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-02-17 16:01:36 +0100

    build: work in progress

--- libs/ui/src/lib/components/toggle-switch/toggle-switch.ts
+++ libs/ui/src/lib/components/toggle-switch/toggle-switch.ts
@@ -12,47 +12,17 @@ import { cn } from '@semantic-components/utils';
   selector: 'button[sc-toggle-switch]',
   imports: [],
   template: `
-    <input class="peer sr-only" id="toggleThree" type="checkbox" />
-    <div class="block h-8 rounded-full bg-gray-3 dark:bg-dark-2 w-14"></div>
-    <div
-      class="absolute flex items-center justify-center w-6 h-6 transition bg-white rounded-full dot dark:bg-dark-5 left-1 top-1 peer-checked:translate-x-full peer-checked:bg-primary"
-    >
-      <span class="hidden active">
-        <svg
-          width="11"
-          height="8"
-          viewBox="0 0 11 8"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M10.0915 0.951972L10.0867 0.946075L10.0813 0.940568C9.90076 0.753564 9.61034 0.753146 9.42927 0.939309L4.16201 6.22962L1.58507 3.63469C1.40401 3.44841 1.11351 3.44879 0.932892 3.63584C0.755703 3.81933 0.755703 4.10875 0.932892 4.29224L0.932878 4.29225L0.934851 4.29424L3.58046 6.95832C3.73676 7.11955 3.94983 7.2 4.1473 7.2C4.36196 7.2 4.55963 7.11773 4.71406 6.9584L10.0468 1.60234C10.2436 1.4199 10.2421 1.1339 10.0915 0.951972ZM4.2327 6.30081L4.2317 6.2998C4.23206 6.30015 4.23237 6.30049 4.23269 6.30082L4.2327 6.30081Z"
-            fill="white"
-            stroke="white"
-            stroke-width="0.4"
-          />
-        </svg>
-      </span>
-      <span class="text-body-color dark:text-light inactive">
-        <svg
-          class="w-4 h-4 stroke-current"
-          fill="none"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M6 18L18 6M6 6l12 12"
-          ></path>
-        </svg>
-      </span>
-    </div>
+    <span
+      class="pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0"
+      data-state="unchecked"
+    ></span>
   `,
   host: {
     '[class]': 'class()',
     role: 'switch',
+    'aria-checked': 'false',
+    'data-state': 'unchecked',
+    value: 'on',
   },
   styles: ``,
   encapsulation: ViewEncapsulation.None,
@@ -63,5 +33,10 @@ export class ScToggleSwitch {
     alias: 'class',
   });
 
-  protected readonly class = computed(() => cn('relative', this.classInput()));
+  protected readonly class = computed(() =>
+    cn(
+      'peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input',
+      this.classInput(),
+    ),
+  );
 }
