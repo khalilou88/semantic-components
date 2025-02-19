4f290d8364ae055ac911e52158b47bf42704eda4
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-02-19 14:54:42 +0100

    build: work in progress

--- apps/showcase/src/app/pages/toast-page.ts
+++ apps/showcase/src/app/pages/toast-page.ts
@@ -4,11 +4,30 @@ import {
   TemplateRef,
   ViewEncapsulation,
   inject,
+  signal,
   viewChild,
 } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 import {
+  ScBreadcrumb,
+  ScBreadcrumbItem,
+  ScBreadcrumbLink,
+  ScBreadcrumbList,
+  ScBreadcrumbPage,
+  ScBreadcrumbSeparator,
   ScButton,
+  ScCard,
+  ScCardContent,
+  ScCodeHighlighter,
+  ScHeading,
+  ScPageDescription,
+  ScPageSubtitle,
+  ScPageTitle,
+  ScTab,
+  ScTabContent,
+  ScTabLabel,
+  ScTabs,
   ScToast,
   ScToastAction,
   ScToastClose,
@@ -17,7 +36,7 @@ import {
   ScToastTitle,
   Toaster,
 } from '@semantic-components/ui';
-import { SiXIcon } from '@semantic-icons/lucide-icons';
+import { SiChevronRightIcon, SiLoaderCircleIcon, SiXIcon } from '@semantic-icons/lucide-icons';
 
 @Component({
   selector: 'app-toast-page',
@@ -30,8 +49,409 @@ import { SiXIcon } from '@semantic-icons/lucide-icons';
     ScToast,
     ScToastContent,
     ScButton,
+    ScButton,
+    SiChevronRightIcon,
+    SiLoaderCircleIcon,
+    ScBreadcrumb,
+    ScBreadcrumbList,
+    ScBreadcrumbItem,
+    ScBreadcrumbLink,
+    ScBreadcrumbPage,
+    ScBreadcrumbSeparator,
+    ScPageTitle,
+    ScPageSubtitle,
+    ScPageDescription,
+    ScTabs,
+    ScTab,
+    ScTabLabel,
+    ScTabContent,
+    ScCard,
+    ScHeading,
+    ScCodeHighlighter,
+    ScCardContent,
+    RouterLink,
   ],
   template: `
+    <div class="relative py-6 lg:gap-10 lg:py-8 xl:grid xl:grid-cols-[1fr_300px] px-4">
+      <div class="mx-auto w-full max-w-3xl">
+        <nav sc-breadcrumb>
+          <ol sc-breadcrumb-list>
+            <li sc-breadcrumb-item><a sc-breadcrumb-link>Components</a></li>
+
+            <li sc-breadcrumb-separator><svg si-chevron-right-icon></svg></li>
+            <li sc-breadcrumb-item>
+              <span sc-breadcrumb-page>Toast</span>
+            </li>
+          </ol>
+        </nav>
+
+        <h1 sc-page-title>Toast</h1>
+
+        <p sc-page-description>A succinct message that is displayed temporarily.</p>
+
+        <section class="my-10" id="">
+          <sc-tabs class="w-[400px]" tabsHeaderClass="grid w-full grid-cols-2">
+            <sc-tab>
+              <sc-tab-label>Preview</sc-tab-label>
+              <sc-tab-content>
+                <div class="overflow-auto" sc-card>
+                  <div class="m-10 flex gap-2 p-0 items-center justify-center" sc-card-content>
+                    <button (click)="showToast()" variant="outline" sc-button type="button">
+                      Add to calendar
+                    </button>
+                  </div>
+                </div>
+
+                <ng-template #toastTemplate>
+                  <div sc-toast>
+                    <div sc-toast-content>
+                      <h2 sc-toast-title>Scheduled: Catch up</h2>
+                      <p sc-toast-description>Friday, February 10, 2023 at 5:57 PM</p>
+                    </div>
+                    <button sc-toast-action type="button">Undo</button>
+                    <button type="button" sc-toast-close>
+                      <svg class="size-4" si-x-icon></svg>
+                      <span class="sr-only">Close</span>
+                    </button>
+                  </div>
+                </ng-template>
+              </sc-tab-content>
+            </sc-tab>
+
+            <sc-tab>
+              <sc-tab-label>Code</sc-tab-label>
+              <sc-tab-content>
+                <div class="overflow-auto" sc-card>
+                  <div class="m-10 flex gap-2 p-0" sc-card-content>
+                    <p>Coming soon</p>
+                  </div>
+                </div>
+              </sc-tab-content>
+            </sc-tab>
+          </sc-tabs>
+        </section>
+
+        <h2 id="usage" sc-page-subtitle>Usage</h2>
+
+        <sc-code-highlighter class="mt-2" [code]="codeSnippet1" language="typescript" />
+
+        <sc-code-highlighter class="mt-2" [code]="codeSnippet2" />
+
+        <h2 class="mb-5" id="examples" sc-page-subtitle>Examples</h2>
+
+        <section class="my-10" id="variants">
+          <h3 class="mb-2" sc-heading level="3">Variants</h3>
+
+          <sc-tabs class="w-[400px]" tabsHeaderClass="grid w-full grid-cols-2">
+            <sc-tab>
+              <sc-tab-label>Preview</sc-tab-label>
+              <sc-tab-content>
+                <div class="overflow-auto" sc-card>
+                  <div class="m-10 flex gap-2 p-0" sc-card-content>
+                    <button sc-button type="button">Primary</button>
+                    <button sc-button variant="secondary" type="button">Secondary</button>
+                    <button sc-button variant="destructive" type="button">Destructive</button>
+                    <button sc-button variant="outline" type="button">Outline</button>
+                    <button sc-button variant="ghost" type="button">Ghost</button>
+                    <button sc-button variant="link" type="button">Link</button>
+                  </div>
+                </div>
+              </sc-tab-content>
+            </sc-tab>
+
+            <sc-tab>
+              <sc-tab-label>Code</sc-tab-label>
+              <sc-tab-content>
+                <sc-code-highlighter [code]="codeSnippet" />
+              </sc-tab-content>
+            </sc-tab>
+          </sc-tabs>
+        </section>
+
+        <section class="my-10" id="small-buttons">
+          <h3 class="mb-2" sc-heading level="3">Small buttons</h3>
+
+          <sc-tabs class="w-[400px]" tabsHeaderClass="grid w-full grid-cols-2">
+            <sc-tab>
+              <sc-tab-label>Preview</sc-tab-label>
+              <sc-tab-content>
+                <div class="overflow-auto" sc-card>
+                  <div class="m-10 flex gap-2 p-0" sc-card-content>
+                    <button sc-button type="button" size="sm">Primary</button>
+                    <button sc-button variant="secondary" type="button" size="sm">Secondary</button>
+                    <button sc-button variant="destructive" type="button" size="sm">
+                      Destructive
+                    </button>
+                    <button sc-button variant="outline" type="button" size="sm">Outline</button>
+                    <button sc-button variant="ghost" type="button" size="sm">Ghost</button>
+                    <button sc-button variant="link" type="button" size="sm">Link</button>
+                  </div>
+                </div>
+              </sc-tab-content>
+            </sc-tab>
+
+            <sc-tab>
+              <sc-tab-label>Code</sc-tab-label>
+              <sc-tab-content>
+                <div class="overflow-auto" sc-card>
+                  <div class="m-10 flex gap-2 p-0" sc-card-content>
+                    <p>Coming soon</p>
+                  </div>
+                </div>
+              </sc-tab-content>
+            </sc-tab>
+          </sc-tabs>
+        </section>
+
+        <section class="my-10" id="large-buttons">
+          <h3 class="mb-2" sc-heading level="3">Large buttons</h3>
+
+          <sc-tabs class="w-[400px]" tabsHeaderClass="grid w-full grid-cols-2">
+            <sc-tab>
+              <sc-tab-label>Preview</sc-tab-label>
+              <sc-tab-content>
+                <div class="overflow-auto" sc-card>
+                  <div class="m-10 flex gap-2 p-0" sc-card-content>
+                    <button sc-button type="button" size="lg">Primary</button>
+                    <button sc-button variant="secondary" type="button" size="lg">Secondary</button>
+                    <button sc-button variant="destructive" type="button" size="lg">
+                      Destructive
+                    </button>
+                    <button sc-button variant="outline" type="button" size="lg">Outline</button>
+                    <button sc-button variant="ghost" type="button" size="lg">Ghost</button>
+                    <button sc-button variant="link" type="button" size="lg">Link</button>
+                  </div>
+                </div>
+              </sc-tab-content>
+            </sc-tab>
+
+            <sc-tab>
+              <sc-tab-label>Code</sc-tab-label>
+              <sc-tab-content>
+                <div class="overflow-auto" sc-card>
+                  <div class="m-10 flex gap-2 p-0" sc-card-content>
+                    <p>Coming soon</p>
+                  </div>
+                </div>
+              </sc-tab-content>
+            </sc-tab>
+          </sc-tabs>
+        </section>
+
+        <button (click)="f()" sc-button>Toggle</button>
+
+        <section class="my-10" id="disabled-buttons">
+          <h3 class="mb-2" sc-heading level="3">Disabled buttons</h3>
+
+          <sc-tabs class="w-[400px]" tabsHeaderClass="grid w-full grid-cols-2">
+            <sc-tab>
+              <sc-tab-label>Preview</sc-tab-label>
+              <sc-tab-content>
+                <div class="overflow-auto" sc-card>
+                  <div class="m-10 flex gap-2 p-0" sc-card-content>
+                    <button [disabled]="d()" sc-button type="button">Primary</button>
+                    <button sc-button variant="secondary" type="button" disabled>Secondary</button>
+                    <button sc-button variant="destructive" type="button" disabled>
+                      Destructive
+                    </button>
+                    <button sc-button variant="outline" type="button" disabled>Outline</button>
+                    <button sc-button variant="ghost" type="button" disabled>Ghost</button>
+                    <button sc-button variant="link" type="button" disabled>Link</button>
+                  </div>
+                </div>
+              </sc-tab-content>
+            </sc-tab>
+
+            <sc-tab>
+              <sc-tab-label>Code</sc-tab-label>
+              <sc-tab-content>
+                <div class="overflow-auto" sc-card>
+                  <div class="m-10 flex gap-2 p-0" sc-card-content>
+                    <p>Coming soon</p>
+                  </div>
+                </div>
+              </sc-tab-content>
+            </sc-tab>
+          </sc-tabs>
+        </section>
+
+        <section class="my-10" id="buttons-with-icons">
+          <h3 class="mb-2" sc-heading level="3">Buttons with icons</h3>
+
+          <sc-tabs class="w-[400px]" tabsHeaderClass="grid w-full grid-cols-2">
+            <sc-tab>
+              <sc-tab-label>Preview</sc-tab-label>
+              <sc-tab-content>
+                <div class="overflow-auto" sc-card>
+                  <div class="m-10 flex gap-2 p-0 items-center" sc-card-content>
+                    <button sc-button type="button" size="icon">
+                      <svg si-chevron-right-icon></svg>
+                    </button>
+                    <button sc-button variant="secondary" type="button" size="icon">
+                      <svg si-chevron-right-icon></svg>
+                    </button>
+                    <button sc-button variant="destructive" type="button" size="icon">
+                      <svg si-chevron-right-icon></svg>
+                    </button>
+                    <button class="size-20" sc-button variant="outline" type="button" size="icon">
+                      <svg si-chevron-right-icon></svg>
+                    </button>
+                    <button sc-button variant="ghost" type="button" size="icon">
+                      <svg si-chevron-right-icon></svg>
+                    </button>
+                    <button sc-button variant="link" type="button" size="icon">
+                      <svg si-chevron-right-icon></svg>
+                    </button>
+                  </div>
+                </div>
+              </sc-tab-content>
+            </sc-tab>
+
+            <sc-tab>
+              <sc-tab-label>Code</sc-tab-label>
+              <sc-tab-content>
+                <div class="overflow-auto" sc-card>
+                  <div class="m-10 flex gap-2 p-0" sc-card-content>
+                    <p>Coming soon</p>
+                  </div>
+                </div>
+              </sc-tab-content>
+            </sc-tab>
+          </sc-tabs>
+        </section>
+
+        <section class="my-10" id="button-with-loading-state">
+          <h3 class="mb-2" sc-heading level="3">Button with loading state</h3>
+
+          <sc-tabs class="w-[400px]" tabsHeaderClass="grid w-full grid-cols-2">
+            <sc-tab>
+              <sc-tab-label>Preview</sc-tab-label>
+              <sc-tab-content>
+                <div class="overflow-auto" sc-card>
+                  <div class="m-10 flex gap-2 p-0" sc-card-content>
+                    <button sc-button disabled>
+                      <svg class="animate-spin" si-loader-circle-icon></svg>
+                      Please wait
+                    </button>
+                  </div>
+                </div>
+              </sc-tab-content>
+            </sc-tab>
+
+            <sc-tab>
+              <sc-tab-label>Code</sc-tab-label>
+              <sc-tab-content>
+                <div class="overflow-auto" sc-card>
+                  <div class="m-10 flex gap-2 p-0" sc-card-content>
+                    <p>Coming soon</p>
+                  </div>
+                </div>
+              </sc-tab-content>
+            </sc-tab>
+          </sc-tabs>
+        </section>
+      </div>
+
+      <div class="hidden text-sm xl:block">
+        <div class="sticky top-20 -mt-6 h-[calc(100vh-3.5rem)] pt-4">
+          <div class="no-scrollbar h-full overflow-auto pb-10">
+            <div class="space-y-2">
+              <p class="font-medium">On This Page</p>
+              <ul class="m-0 list-none">
+                <!--li class="mt-0 pt-2">
+                  <a
+                    class="inline-block font-medium text-foreground no-underline transition-colors hover:text-foreground"
+                    href="#installation"
+                  >
+                    Installation
+                  </a>
+                </li-->
+                <li class="mt-0 pt-2">
+                  <a
+                    class="inline-block text-muted-foreground no-underline transition-colors hover:text-foreground"
+                    routerLink="."
+                    fragment="usage"
+                  >
+                    Usage
+                  </a>
+                </li>
+                <li class="mt-0 pt-2">
+                  <a
+                    class="inline-block text-muted-foreground no-underline transition-colors hover:text-foreground"
+                    routerLink="."
+                    fragment="examples"
+                  >
+                    Examples
+                  </a>
+                  <ul class="m-0 list-none pl-4">
+                    <li class="mt-0 pt-2">
+                      <a
+                        class="inline-block text-muted-foreground no-underline transition-colors hover:text-foreground"
+                        routerLink="."
+                        fragment="variants"
+                      >
+                        Variants
+                      </a>
+                    </li>
+
+                    <li class="mt-0 pt-2">
+                      <a
+                        class="inline-block text-muted-foreground no-underline transition-colors hover:text-foreground"
+                        routerLink="."
+                        fragment="small-buttons"
+                      >
+                        Small buttons
+                      </a>
+                    </li>
+
+                    <li class="mt-0 pt-2">
+                      <a
+                        class="inline-block text-muted-foreground no-underline transition-colors hover:text-foreground"
+                        routerLink="."
+                        fragment="large-buttons"
+                      >
+                        Large buttons
+                      </a>
+                    </li>
+
+                    <li class="mt-0 pt-2">
+                      <a
+                        class="inline-block text-muted-foreground no-underline transition-colors hover:text-foreground"
+                        routerLink="."
+                        fragment="disabled-buttons"
+                      >
+                        Disabled buttons
+                      </a>
+                    </li>
+
+                    <li class="mt-0 pt-2">
+                      <a
+                        class="inline-block text-muted-foreground no-underline transition-colors hover:text-foreground"
+                        routerLink="."
+                        fragment="buttons-with-icons"
+                      >
+                        Buttons with icons
+                      </a>
+                    </li>
+
+                    <li class="mt-0 pt-2">
+                      <a
+                        class="inline-block text-muted-foreground no-underline transition-colors hover:text-foreground"
+                        routerLink="."
+                        fragment="button-with-loading-state"
+                      >
+                        Button with loading state
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <div class="m-10">
       <div class="flex space-x-2">
         <button (click)="showToast()" sc-button type="button">Show Toast 1</button>
@@ -42,20 +462,6 @@ import { SiXIcon } from '@semantic-icons/lucide-icons';
 
         <button (click)="showToast4()" sc-button type="button">Show Toast 4</button>
       </div>
-
-      <ng-template #toastTemplate>
-        <div sc-toast>
-          <div sc-toast-content>
-            <h2 sc-toast-title>Scheduled: Catch up</h2>
-            <p sc-toast-description>Friday, February 10, 2023 at 5:57 PM</p>
-          </div>
-          <button sc-toast-action type="button">Undo</button>
-          <button type="button" sc-toast-close>
-            <svg class="size-4" si-x-icon></svg>
-            <span class="sr-only">Close</span>
-          </button>
-        </div>
-      </ng-template>
 
       <ng-template #toastTemplate2>
         <div sc-toast>
@@ -100,6 +506,9 @@ import { SiXIcon } from '@semantic-icons/lucide-icons';
       </ng-template>
     </div>
   `,
+  host: {
+    '[class]': 'class()',
+  },
   styles: ``,
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -127,5 +536,24 @@ export default class ToastPage {
 
   protected showToast4() {
     this.toaster.show(this.toastTemplate4());
+  }
+
+  class = signal<string>('block w-full');
+
+  codeSnippet1 = `import { ScButton } from '@semantic-components/ui';`;
+
+  codeSnippet2 = `<button sc-button type="button">Primary</button>`;
+
+  codeSnippet = `<button sc-button type="button">Primary</button>
+    <button sc-button variant="secondary" type="button">Secondary</button>
+    <button sc-button variant="destructive" type="button">Destructive</button>
+    <button sc-button variant="outline" type="button">Outline</button>
+    <button sc-button variant="ghost" type="button">Ghost</button>
+    <button sc-button variant="link" type="button">Link</button>`;
+
+  d = signal(true);
+
+  f() {
+    this.d.update((v) => !v);
   }
 }
