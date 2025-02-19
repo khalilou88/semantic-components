086b98388394616460ac34f4bcbff87ffa24f85f
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-02-19 17:00:58 +0100

    build: work in progress

--- libs/ui/src/lib/components/toast/toast-container.ts
+++ libs/ui/src/lib/components/toast/toast-container.ts
@@ -5,9 +5,12 @@ import {
   Injector,
   TemplateRef,
   ViewEncapsulation,
+  computed,
   inject,
   input,
 } from '@angular/core';
+
+import { cn } from '@semantic-components/utils';
 
 @Component({
   selector: 'sc-toast-container',
@@ -17,11 +20,20 @@ import {
       <ng-container *ngTemplateOutlet="templateRef(); injector: injector"></ng-container>
     }
   `,
+  host: {
+    '[class]': 'class()',
+  },
   styles: ``,
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ScToastContainer {
+  readonly classInput = input<string>('', {
+    alias: 'class',
+  });
+
+  protected readonly class = computed(() => cn('block md:max-w-[420px]', this.classInput()));
+
   readonly templateRef = input<TemplateRef<unknown> | null>(null);
 
   protected readonly injector = inject(Injector);
