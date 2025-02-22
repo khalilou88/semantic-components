7f1b7b979568cb8d1d9a1b77ae9ed4d0a4ddde8f
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-02-22 19:20:13 +0100

    build: work in progress

--- apps/showcase/src/app/pages/paginator-page.ts
+++ apps/showcase/src/app/pages/paginator-page.ts
@@ -95,23 +95,13 @@ import {
 
         <p sc-page-description>Pagination with page navigation, next and previous links.</p>
 
-        <h2 id="usage" sc-page-subtitle>Usage</h2>
-
-        <sc-code-highlighter class="mt-2" [code]="codeSnippet1" language="typescript" />
-
-        <sc-code-highlighter class="mt-2" [code]="codeSnippet2" />
-
-        <h2 class="mb-5" id="examples" sc-page-subtitle>Examples</h2>
-
-        <section class="my-10" id="variants">
-          <h3 class="mb-2" sc-heading level="3">Pagination first variant</h3>
-
+        <section class="my-10">
           <sc-tabs class="w-[400px]" tabsHeaderClass="grid w-full grid-cols-2">
             <sc-tab>
               <sc-tab-label>Preview</sc-tab-label>
               <sc-tab-content>
                 <div class="overflow-auto" sc-card>
-                  <div class="m-10 flex gap-2 p-0" sc-card-content>
+                  <div class="m-10 flex gap-2 p-0 items-center justify-center" sc-card-content>
                     <div
                       #pp="scPaginator"
                       [currentPage]="currentPage()"
@@ -165,13 +155,21 @@ import {
             <sc-tab>
               <sc-tab-label>Code</sc-tab-label>
               <sc-tab-content>
-                <sc-code-highlighter [code]="codeSnippet" />
+                <sc-code-highlighter [code]="templateCodeSnippet" />
               </sc-tab-content>
             </sc-tab>
           </sc-tabs>
         </section>
 
-        <section class="my-10" id="small-buttons">
+        <h2 id="usage" sc-page-subtitle>Usage</h2>
+
+        <sc-code-highlighter class="mt-2" [code]="importCodeSnippet" language="typescript" />
+
+        <sc-code-highlighter class="mt-2" [code]="templateCodeSnippet" />
+
+        <h2 class="mb-5" id="examples" sc-page-subtitle>Examples</h2>
+
+        <section class="my-10" id="pagination-second-variant">
           <h3 class="mb-2" sc-heading level="3">Pagination second variant</h3>
 
           <sc-tabs class="w-[400px]" tabsHeaderClass="grid w-full grid-cols-2">
@@ -179,7 +177,7 @@ import {
               <sc-tab-label>Preview</sc-tab-label>
               <sc-tab-content>
                 <div class="overflow-auto" sc-card>
-                  <div class="m-10 flex gap-2 p-0" sc-card-content>
+                  <div class="m-10 flex gap-2 p-0 items-center justify-center" sc-card-content>
                     <div
                       #p="scPaginator"
                       [currentPage]="currentPage()"
@@ -255,7 +253,7 @@ import {
           </sc-tabs>
         </section>
 
-        <section class="my-10" id="large-buttons">
+        <section class="my-10" id="items-per-page">
           <h3 class="mb-2" sc-heading level="3">Items per page</h3>
 
           <sc-tabs class="w-[400px]" tabsHeaderClass="grid w-full grid-cols-2">
@@ -263,7 +261,7 @@ import {
               <sc-tab-label>Preview</sc-tab-label>
               <sc-tab-content>
                 <div class="overflow-auto" sc-card>
-                  <div class="m-10 flex gap-2 p-0" sc-card-content>
+                  <div class="m-10 flex gap-2 p-0 items-center justify-center" sc-card-content>
                     <div
                       class="flex justify-between"
                       #p="scPaginator"
@@ -345,7 +343,7 @@ import {
           </sc-tabs>
         </section>
 
-        <section class="my-10" id="buttons-with-icons">
+        <section class="my-10" id="pagination-infos">
           <h3 class="mb-2" sc-heading level="3">Pagination infos</h3>
 
           <sc-tabs class="w-[400px]" tabsHeaderClass="grid w-full grid-cols-2">
@@ -353,7 +351,7 @@ import {
               <sc-tab-label>Preview</sc-tab-label>
               <sc-tab-content>
                 <div class="overflow-auto" sc-card>
-                  <div class="m-10 flex gap-2 p-0 items-center" sc-card-content>
+                  <div class="m-10 flex gap-2 p-0 items-center justify-center" sc-card-content>
                     <div
                       #pag="scPaginator"
                       [currentPage]="currentPage()"
@@ -396,14 +394,6 @@ import {
             <div class="space-y-2">
               <p class="font-medium">On This Page</p>
               <ul class="m-0 list-none">
-                <!--li class="mt-0 pt-2">
-                  <a
-                    class="inline-block font-medium text-foreground no-underline transition-colors hover:text-foreground"
-                    href="#installation"
-                  >
-                    Installation
-                  </a>
-                </li-->
                 <li class="mt-0 pt-2">
                   <a
                     class="inline-block text-muted-foreground no-underline transition-colors hover:text-foreground"
@@ -426,9 +416,9 @@ import {
                       <a
                         class="inline-block text-muted-foreground no-underline transition-colors hover:text-foreground"
                         routerLink="."
-                        fragment="variants"
+                        fragment="pagination-second-variant"
                       >
-                        Variants
+                        Pagination second variant
                       </a>
                     </li>
 
@@ -436,9 +426,9 @@ import {
                       <a
                         class="inline-block text-muted-foreground no-underline transition-colors hover:text-foreground"
                         routerLink="."
-                        fragment="small-buttons"
+                        fragment="items-per-page"
                       >
-                        Small buttons
+                        Items per page
                       </a>
                     </li>
 
@@ -446,39 +436,9 @@ import {
                       <a
                         class="inline-block text-muted-foreground no-underline transition-colors hover:text-foreground"
                         routerLink="."
-                        fragment="large-buttons"
+                        fragment="pagination-infos"
                       >
-                        Large buttons
-                      </a>
-                    </li>
-
-                    <li class="mt-0 pt-2">
-                      <a
-                        class="inline-block text-muted-foreground no-underline transition-colors hover:text-foreground"
-                        routerLink="."
-                        fragment="disabled-buttons"
-                      >
-                        Disabled buttons
-                      </a>
-                    </li>
-
-                    <li class="mt-0 pt-2">
-                      <a
-                        class="inline-block text-muted-foreground no-underline transition-colors hover:text-foreground"
-                        routerLink="."
-                        fragment="buttons-with-icons"
-                      >
-                        Buttons with icons
-                      </a>
-                    </li>
-
-                    <li class="mt-0 pt-2">
-                      <a
-                        class="inline-block text-muted-foreground no-underline transition-colors hover:text-foreground"
-                        routerLink="."
-                        fragment="button-with-loading-state"
-                      >
-                        Button with loading state
+                        Pagination infos
                       </a>
                     </li>
                   </ul>
@@ -509,20 +469,63 @@ export default class PaginatorPage {
 
   class = signal<string>('block w-full');
 
-  codeSnippet1 = `import { ScButton } from '@semantic-components/ui';`;
+  importCodeSnippet = `import {
+  ScPaginator,
+  ScPagination,
+  ScPageEvent,
+  ScPageSizeSelect,
+  ScPaginationEllipsis,
+  ScPaginationFirst,
+  ScPaginationLast,
+  ScPaginationLink,
+  ScPaginationList,
+  ScPaginationNext,
+  ScPaginationPrevious,
+} from '@semantic-components/ui';`;
 
-  codeSnippet2 = `<button sc-button type="button">Primary</button>`;
+  templateCodeSnippet = `<div
+  #pp="scPaginator"
+  [currentPage]="currentPage()"
+  [pageSize]="pageSize()"
+  [totalSize]="totalSize()"
+  (pageChanged)="setPageEvent($event)"
+  paginationActiveLinkVariant="outline"
+  paginationLinkVariant="ghost"
+  sc-paginator
+>
+  <nav sc-pagination>
+    <ul sc-pagination-list>
+      <li>
+        <a sc-pagination-previous>
+          <svg si-chevron-left-icon></svg>
+          <span>Previous</span>
+          <span class="sr-only">Previous page</span>
+        </a>
+      </li>
 
-  codeSnippet = `<button sc-button type="button">Primary</button>
-  <button sc-button variant="secondary" type="button">Secondary</button>
-  <button sc-button variant="destructive" type="button">Destructive</button>
-  <button sc-button variant="outline" type="button">Outline</button>
-  <button sc-button variant="ghost" type="button">Ghost</button>
-  <button sc-button variant="link" type="button">Link</button>`;
+      @for (page of pp.pageRanges(); track $index) {
+        <li>
+          @if (page === '...') {
+            <span sc-pagination-ellipsis>
+              <svg class="size-4" si-ellipsis-icon></svg>
+              <span class="sr-only">More pages</span>
+            </span>
+          } @else {
+            <a [page]="page" sc-pagination-link>
+              {{ page }}
+            </a>
+          }
+        </li>
+      }
 
-  d = signal(true);
-
-  f() {
-    this.d.update((v) => !v);
-  }
+      <li>
+        <a sc-pagination-next>
+          <span>Next</span>
+          <svg si-chevron-right-icon></svg>
+          <span class="sr-only">Next page</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+</div>`;
 }
