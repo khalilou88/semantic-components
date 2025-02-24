758cbd5d341ae78b5b8f13b71f659ba5dc11435f
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-02-24 17:34:48 +0100

    build: work in progress

--- apps/showcase/src/app/pages/switch-page.ts
+++ apps/showcase/src/app/pages/switch-page.ts
@@ -1,40 +1,131 @@
-import { JsonPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
-import { ScSwitch } from '@semantic-components/ui';
+import {
+  ScBreadcrumb,
+  ScBreadcrumbItem,
+  ScBreadcrumbLink,
+  ScBreadcrumbList,
+  ScBreadcrumbPage,
+  ScBreadcrumbSeparator,
+  ScCard,
+  ScCardContent,
+  ScCodeHighlighter,
+  ScPageDescription,
+  ScPageSubtitle,
+  ScPageTitle,
+  ScSwitch,
+  ScTab,
+  ScTabContent,
+  ScTabLabel,
+  ScTabs,
+} from '@semantic-components/ui';
+import { SiChevronRightIcon } from '@semantic-icons/lucide-icons';
 
 @Component({
   selector: 'app-switch-page',
-  imports: [ScSwitch, ReactiveFormsModule, JsonPipe],
+  imports: [
+    ScSwitch,
+    ReactiveFormsModule,
+    SiChevronRightIcon,
+    ScBreadcrumb,
+    ScBreadcrumbList,
+    ScBreadcrumbItem,
+    ScBreadcrumbLink,
+    ScBreadcrumbPage,
+    ScBreadcrumbSeparator,
+    ScPageTitle,
+    ScPageSubtitle,
+    ScPageDescription,
+    ScTabs,
+    ScTab,
+    ScTabLabel,
+    ScTabContent,
+    ScCard,
+    ScCodeHighlighter,
+    ScCardContent,
+    RouterLink,
+  ],
   template: `
-    <div class="m-10">
-      <br />
-      <br />
-      <br />
-      <form [formGroup]="switchForm">
-        <div class="flex items-center space-x-2">
-          <input sc-switch formControlName="switch" />
+    <div class="relative py-6 lg:gap-10 lg:py-8 xl:grid xl:grid-cols-[1fr_300px] px-4">
+      <div class="mx-auto w-full max-w-3xl">
+        <nav sc-breadcrumb>
+          <ol sc-breadcrumb-list>
+            <li sc-breadcrumb-item><a sc-breadcrumb-link>Components</a></li>
 
-          Airplane Mode
+            <li sc-breadcrumb-separator><svg si-chevron-right-icon></svg></li>
+            <li sc-breadcrumb-item>
+              <span sc-breadcrumb-page>Switch</span>
+            </li>
+          </ol>
+        </nav>
+
+        <h1 sc-page-title>Switch</h1>
+
+        <p sc-page-description>
+          A control that allows the user to toggle between checked and not checked.
+        </p>
+
+        <section class="my-10">
+          <sc-tabs class="w-[400px]" tabsHeaderClass="grid w-full grid-cols-2">
+            <sc-tab>
+              <sc-tab-label>Preview</sc-tab-label>
+              <sc-tab-content>
+                <div class="overflow-auto" sc-card>
+                  <div class="m-10 flex gap-2 p-0 items-center justify-center" sc-card-content>
+                    <form [formGroup]="switchForm">
+                      <div class="flex items-center space-x-2">
+                        <input sc-switch formControlName="switch" />
+                        Airplane Mode
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </sc-tab-content>
+            </sc-tab>
+
+            <sc-tab>
+              <sc-tab-label>Code</sc-tab-label>
+              <sc-tab-content>
+                <sc-code-highlighter [code]="templateCodeSnippet" />
+              </sc-tab-content>
+            </sc-tab>
+          </sc-tabs>
+        </section>
+
+        <h2 id="usage" sc-page-subtitle>Usage</h2>
+
+        <sc-code-highlighter class="mt-2" [code]="importCodeSnippet" language="typescript" />
+
+        <sc-code-highlighter class="mt-2" [code]="templateCodeSnippet" />
+      </div>
+
+      <div class="hidden text-sm xl:block">
+        <div class="sticky top-20 -mt-6 h-[calc(100vh-3.5rem)] pt-4">
+          <div class="no-scrollbar h-full overflow-auto pb-10">
+            <div class="space-y-2">
+              <p class="font-medium">On This Page</p>
+              <ul class="m-0 list-none">
+                <li class="mt-0 pt-2">
+                  <a
+                    class="inline-block text-muted-foreground no-underline transition-colors hover:text-foreground"
+                    routerLink="."
+                    fragment="usage"
+                  >
+                    Usage
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-
-        <br />
-        <br />
-
-        <div class="flex items-center space-x-2">
-          <input [disabled]="true" sc-switch formControlName="switch2" />
-
-          Airplane Mode 2
-        </div>
-      </form>
-
-      <br />
-      <br />
-      <br />
-      {{ switchForm.value | json }}
+      </div>
     </div>
   `,
+  host: {
+    '[class]': 'class()',
+  },
   styles: ``,
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -44,4 +135,12 @@ export default class SwitchPage {
     switch: new FormControl(),
     switch2: new FormControl(),
   });
+
+  class = signal<string>('block w-full');
+
+  importCodeSnippet = `import {
+  ScSwitch,
+} from '@semantic-components/ui';`;
+
+  templateCodeSnippet = `<input sc-switch formControlName="switch" />`;
 }
