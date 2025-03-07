f2c480249e02252062cbac79c5ba48aa09dbd7f7
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-03-08 00:48:50 +0100

    build: work in progress

--- libs/ui/src/lib/components/switch/switch.ts
+++ libs/ui/src/lib/components/switch/switch.ts
@@ -52,31 +52,34 @@ export class ScSwitch implements ControlValueAccessor {
     cn(
       'appearance-none',
       'w-11 h-6 relative cursor-pointer inline-block',
-      'focus:outline-0 dark:focus:outline-0',
-      'border-0 dark:border-0',
-      'focus:ring-offset-transparent dark:focus:ring-offset-transparent',
-      'focus:ring-transparent dark:focus:ring-transparent',
-      'focus-within:ring-0 dark:focus-within:ring-0',
-      'focus:shadow-none dark:focus:shadow-none',
+      'focus:outline-0',
+      'border-0',
+      'focus:ring-offset-transparent',
+      'focus:ring-transparent',
+      'focus-within:ring-0',
+      'focus:shadow-none',
 
-      'after:absolute before:absolute',
-      'after:top-0 before:top-0',
-      'after:block before:inline-block',
-      'before:rounded-full after:rounded-full',
+      'relative', // Make sure positioning context is established
 
-      "after:content-[''] after:w-5 after:h-5 after:mt-0.5 after:ml-0.5",
-      'after:shadow-md after:duration-100',
+      // Use the new pseudo-element syntax if it changed
+      '[&::after]:absolute [&::before]:absolute',
+      '[&::after]:top-0 [&::before]:top-0',
+      '[&::after]:block [&::before]:inline-block',
+      '[&::before]:rounded-full [&::after]:rounded-full',
 
-      "before:content-[''] before:w-10 before:h-full",
-      'before:shadow-[inset_0_0_#000]',
+      "[&::after]:content-[''] [&::after]:w-5 [&::after]:h-5 [&::after]:mt-0.5 [&::after]:ml-0.5",
+      '[&::after]:shadow-md [&::after]:duration-100',
 
-      'after:bg-white dark:after:bg-gray-50',
-      'before:bg-gray-300 dark:before:bg-gray-600',
-      'before:checked:bg-lime-500 dark:before:checked:bg-lime-500',
-      'checked:after:duration-300 checked:after:translate-x-4',
+      "[&::before]:content-[''] [&::before]:w-10 [&::before]:h-full",
+      '[&::before]:shadow-[inset_0_0_#000]',
 
-      'disabled:after:bg-opacity-75 disabled:cursor-not-allowed',
-      'disabled:checked:before:bg-opacity-40',
+      '[&::after]:bg-white dark:[&::after]:bg-gray-50',
+      '[&::before]:bg-gray-300 dark:[&::before]:bg-gray-600',
+      '[&:checked]:[&::before]:bg-primary dark:[&:checked]:[&::before]:bg-primary',
+      '[&:checked]:[&::after]:duration-300 [&:checked]:[&::after]:translate-x-4',
+
+      '[&:disabled]:[&::after]:bg-opacity-75 [&:disabled]:cursor-not-allowed',
+      '[&:disabled]:checked:[&::before]:bg-opacity-40',
 
       this.classInput(),
     ),
