c4b70c6bb4d8e06a560d36f1bc70f83c199fb769
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-03-13 16:48:03 +0100

    build: work in progress

---
+++ libs/ui/src/lib/components/input-number/input-number.ts
@@ -0,0 +1,30 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+  computed,
+  input,
+} from '@angular/core';
+
+import { cn } from '@semantic-components/utils';
+
+@Component({
+  selector: 'input[sc-input-number]',
+  imports: [],
+  template: `
+    <ng-content />
+  `,
+  host: {
+    '[class]': 'class()',
+  },
+  styles: ``,
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ScInputNumber {
+  readonly classInput = input<string>('', {
+    alias: 'class',
+  });
+
+  protected readonly class = computed(() => cn('', this.classInput()));
+}
