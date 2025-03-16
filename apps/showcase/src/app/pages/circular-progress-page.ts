1ae0f8a797262f631fd5fd5da3bb6a52265b51d1
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-03-16 01:05:57 +0100

    build: work in progress

--- apps/showcase/src/app/pages/circular-progress-page.ts
+++ apps/showcase/src/app/pages/circular-progress-page.ts
@@ -5,29 +5,128 @@ import {
   ViewEncapsulation,
   signal,
 } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
-import { ScCircularProgress, ScProgress } from '@semantic-components/ui';
+import {
+  ScBreadcrumb,
+  ScBreadcrumbItem,
+  ScBreadcrumbLink,
+  ScBreadcrumbList,
+  ScBreadcrumbPage,
+  ScBreadcrumbSeparator,
+  ScCard,
+  ScCardContent,
+  ScCircularProgress,
+  ScCodeHighlighter,
+  ScPageDescription,
+  ScPageSubtitle,
+  ScPageTitle,
+  ScTab,
+  ScTabContent,
+  ScTabLabel,
+  ScTabs,
+} from '@semantic-components/ui';
+import { SiChevronRightIcon } from '@semantic-icons/lucide-icons';
 
 @Component({
   selector: 'app-circular-progress-page',
-  imports: [ScProgress, ScCircularProgress],
+  imports: [
+    ScCircularProgress,
+    ReactiveFormsModule,
+    SiChevronRightIcon,
+    ScBreadcrumb,
+    ScBreadcrumbList,
+    ScBreadcrumbItem,
+    ScBreadcrumbLink,
+    ScBreadcrumbPage,
+    ScBreadcrumbSeparator,
+    ScPageTitle,
+    ScPageSubtitle,
+    ScPageDescription,
+    ScTabs,
+    ScTab,
+    ScTabLabel,
+    ScTabContent,
+    ScCard,
+    ScCodeHighlighter,
+    ScCardContent,
+    RouterLink,
+  ],
   template: `
-    <div class="m-10">
-      <sc-progress [value]="progress()" />
+    <div class="relative py-6 lg:gap-10 lg:py-8 xl:grid xl:grid-cols-[1fr_300px] px-4">
+      <div class="mx-auto w-full max-w-3xl">
+        <nav sc-breadcrumb>
+          <ol sc-breadcrumb-list>
+            <li sc-breadcrumb-item><a sc-breadcrumb-link>Components</a></li>
 
-      <br />
-      <br />
-      <br />
+            <li sc-breadcrumb-separator><svg si-chevron-right-icon></svg></li>
+            <li sc-breadcrumb-item>
+              <span sc-breadcrumb-page>Circular Progress</span>
+            </li>
+          </ol>
+        </nav>
 
-      <sc-progress mode="indeterminate" />
+        <h1 sc-page-title>Circular Progress</h1>
 
-      <br />
-      <br />
-      <br />
+        <p sc-page-description>
+          Displays a circular indicator showing the completion progress of a task.
+        </p>
 
-      <sc-circular-progress [value]="progress()" />
+        <section class="my-10">
+          <sc-tabs class="w-[400px]" tabsHeaderClass="grid w-full grid-cols-2">
+            <sc-tab>
+              <sc-tab-label>Preview</sc-tab-label>
+              <sc-tab-content>
+                <div class="overflow-auto" sc-card>
+                  <div class="m-10 flex gap-2 p-0 items-center justify-center" sc-card-content>
+                    <sc-circular-progress [value]="progress()" />
+                  </div>
+                </div>
+              </sc-tab-content>
+            </sc-tab>
+
+            <sc-tab>
+              <sc-tab-label>Code</sc-tab-label>
+              <sc-tab-content>
+                <sc-code-highlighter [code]="templateCodeSnippet" />
+              </sc-tab-content>
+            </sc-tab>
+          </sc-tabs>
+        </section>
+
+        <h2 id="usage" sc-page-subtitle>Usage</h2>
+
+        <sc-code-highlighter class="mt-2" [code]="importCodeSnippet" language="typescript" />
+
+        <sc-code-highlighter class="mt-2" [code]="templateCodeSnippet" />
+      </div>
+
+      <div class="hidden text-sm xl:block">
+        <div class="sticky top-20 -mt-6 h-[calc(100vh-3.5rem)] pt-4">
+          <div class="no-scrollbar h-full overflow-auto pb-10">
+            <div class="space-y-2">
+              <p class="font-medium">On This Page</p>
+              <ul class="m-0 list-none">
+                <li class="mt-0 pt-2">
+                  <a
+                    class="inline-block text-muted-foreground no-underline transition-colors hover:text-foreground"
+                    routerLink="."
+                    fragment="usage"
+                  >
+                    Usage
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   `,
+  host: {
+    '[class]': 'class()',
+  },
   styles: ``,
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -38,4 +137,12 @@ export default class CircularProgressPage implements OnInit {
   ngOnInit() {
     setTimeout(() => this.progress.set(50), 3000);
   }
+
+  class = signal<string>('block w-full');
+
+  importCodeSnippet = `import {
+  ScCircularProgress,
+} from '@semantic-components/ui';`;
+
+  templateCodeSnippet = `<sc-circular-progress [value]="progress()" />`;
 }
