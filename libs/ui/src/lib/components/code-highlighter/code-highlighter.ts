d018a997cc51484ecc74b7a1920e73d339fccca6
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-03-22 16:58:42 +0100

    build: work in progress

--- libs/ui/src/lib/components/code-highlighter/code-highlighter.ts
+++ libs/ui/src/lib/components/code-highlighter/code-highlighter.ts
@@ -12,34 +12,19 @@ import {
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 import { cn } from '@semantic-components/utils';
+import { SiCopyIcon } from '@semantic-icons/lucide-icons';
 
+import { ScButton } from '../button';
 import { ShikiService, defaultThemes } from './shiki.service';
 
 @Component({
   selector: 'sc-code-highlighter',
-  imports: [ClipboardModule],
+  imports: [ClipboardModule, SiCopyIcon, ScButton],
   template: `
     <div [innerHTML]="highlightedCode()"></div>
 
-    <button
-      class="absolute top-2 right-2 inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-secondary hover:text-secondary-foreground h-6 w-6"
-      [cdkCopyToClipboard]="code()"
-    >
-      <svg
-        class="h-4 w-4"
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      >
-        <rect width="14" height="14" x="8" y="8" rx="2" ry="2"></rect>
-        <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"></path>
-      </svg>
+    <button class="absolute top-2 right-2" [cdkCopyToClipboard]="code()" sc-button variant="ghost">
+      <svg class="h-4 w-4" si-copy-icon></svg>
     </button>
   `,
   host: {
