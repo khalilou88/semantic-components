2d465c12591b6f1b0a507bfdeda89ccd7465cb21
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-03-22 04:23:05 +0100

    build: work in progress

--- apps/app/src/app/components/preview-code-tabs/preview-code-tabs.ts
+++ apps/app/src/app/components/preview-code-tabs/preview-code-tabs.ts
@@ -23,8 +23,33 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
             Code
           </button>
         </div>
-        <div class="p-4 data-[state=inactive]:hidden" data-state="active" data-tab-content="tab1">
-          <p>This is the Account tab content.</p>
+        <div class="py-4 data-[state=inactive]:hidden" data-state="active" data-tab-content="tab1">
+          <div class="relative">
+            <pre
+              class="text-sm overflow-x-auto rounded-lg border bg-background px-4 py-3 font-mono text-card-foreground whitespace-pre-wrap break-words"
+            >
+            <ng-content />
+          </pre>
+            <button
+              class="absolute top-2 right-2 inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-secondary hover:text-secondary-foreground h-6 w-6"
+            >
+              <svg
+                class="h-4 w-4"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <rect width="14" height="14" x="8" y="8" rx="2" ry="2"></rect>
+                <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"></path>
+              </svg>
+            </button>
+          </div>
         </div>
         <div class="p-4 data-[state=inactive]:hidden" data-state="inactive" data-tab-content="tab2">
           <p>This is the Password tab content.</p>
--- apps/app/src/app/docs/components/button/page.ts
+++ apps/app/src/app/docs/components/button/page.ts
@@ -1,12 +1,33 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
+import { ScButton } from '@semantic-components/ui';
+
 import { PreviewCodeTabs } from '../../../components/preview-code-tabs/preview-code-tabs';
 
 @Component({
   selector: 'app-page',
-  imports: [PreviewCodeTabs],
+  imports: [PreviewCodeTabs, ScButton],
   template: `
-    <app-preview-code-tabs></app-preview-code-tabs>
+    <app-preview-code-tabs>
+      <div class="flex flex-wrap gap-2">
+        <button sc-button variant="primary">Primary</button>
+
+        <!-- Secondary Button -->
+        <button sc-button variant="secondary">Secondary</button>
+
+        <!-- Destructive Button -->
+        <button sc-button variant="destructive">Destructive</button>
+
+        <!-- Outline Button -->
+        <button sc-button variant="outline">Outline</button>
+
+        <!-- Ghost Button -->
+        <button sc-button variant="ghost">Ghost</button>
+
+        <!-- Link Button -->
+        <button sc-button variant="link">Link</button>
+      </div>
+    </app-preview-code-tabs>
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
