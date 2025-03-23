e405a883540ad37b126a91703bd8f69555644cf4
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-03-23 18:12:00 +0100

    build: work in progress

--- libs/ui/src/lib/components/checkbox-group/checkbox-group.ts
+++ libs/ui/src/lib/components/checkbox-group/checkbox-group.ts
@@ -4,9 +4,12 @@ import {
   ViewEncapsulation,
   booleanAttribute,
   computed,
+  forwardRef,
   input,
+  linkedSignal,
   model,
 } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 import { cn } from '@semantic-components/utils';
 
@@ -22,17 +25,47 @@ import { cn } from '@semantic-components/utils';
   styles: ``,
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => ScCheckboxGroup),
+      multi: true,
+    },
+  ],
 })
-export class ScCheckboxGroup {
+export class ScCheckboxGroup implements ControlValueAccessor {
   readonly classInput = input<string>('', {
     alias: 'class',
   });
 
   protected readonly class = computed(() => cn('space-y-2', this.classInput()));
 
-  readonly disabled = input<boolean, unknown>(false, {
+  readonly disabledInput = input<boolean, unknown>(false, {
+    alias: 'disabled',
     transform: booleanAttribute,
   });
+  readonly disabled = linkedSignal(() => this.disabledInput());
 
   readonly value = model<unknown>(undefined);
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  private onChange: (value: any) => void = () => {};
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  private onTouched: () => void = () => {};
+
+  writeValue(obj: any): void {
+    this.value.set(obj);
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled.set(isDisabled);
+  }
 }
