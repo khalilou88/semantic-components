d832f7cf89eb661dda3011e74ebb6cdc7613e898
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-03-23 18:42:30 +0100

    build: work in progress

--- libs/ui/src/lib/components/checkbox/checkbox2.ts
+++ libs/ui/src/lib/components/checkbox/checkbox2.ts
@@ -27,7 +27,12 @@ import { cn } from '@semantic-components/utils';
     '[type]': 'type()',
     '[class]': 'class()',
   },
-  styles: ``,
+  styles: `
+    :root {
+      --checkbox-checked-bg: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='white' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='lucide lucide-check'%3E%3Cpath d='M20 6 9 17l-5-5'/%3E%3C/svg%3E");
+      --checkbox-indeterminate-bg: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='white' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='lucide lucide-minus'%3E%3Cpath d='M5 12h14'/%3E%3C/svg%3E");
+    }
+  `,
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
