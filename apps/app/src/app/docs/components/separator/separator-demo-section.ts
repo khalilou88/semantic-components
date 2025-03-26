71e8d37fad25ae123bedf4a61a11ad165da8fdcf
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-03-26 21:09:27 +0100

    build: work in progress

--- apps/app/src/app/docs/components/separator/separator-demo-section.ts
+++ apps/app/src/app/docs/components/separator/separator-demo-section.ts
@@ -20,97 +20,30 @@ export class SeparatorDemoSection {
 
   readonly level = input<'2' | '3'>('2');
 
-  protected readonly code = `import { ChangeDetectionStrategy, Component, ViewEncapsulation, signal } from '@angular/core';
-
-import {
-  ScPageEvent,
-  ScPagination,
-  ScPaginationEllipsis,
-  ScPaginationLink,
-  ScPaginationList,
-  ScPaginationNext,
-  ScPaginationPrevious,
-  ScPaginator,
-} from '@semantic-components/ui';
-import {
-  SiChevronLeftIcon,
-  SiChevronRightIcon,
-  SiEllipsisIcon,
-} from '@semantic-icons/lucide-icons';
-
-@Component({
-  selector: 'app-pagination-demo',
-  imports: [
-    ScPaginator,
-    ScPagination,
-    SiChevronLeftIcon,
-    SiChevronRightIcon,
-    ScPaginationPrevious,
-    ScPaginationNext,
-    ScPaginationLink,
-    SiEllipsisIcon,
-    ScPaginationEllipsis,
-    ScPaginationList,
-    SiChevronRightIcon,
-  ],
-  template: \`
-    <sc-paginator
-      #paginator="scPaginator"
-      [currentPage]="currentPage()"
-      [pageSize]="pageSize()"
-      [totalSize]="totalSize()"
-      (pageChanged)="setPageEvent($event)"
-      paginationActiveLinkVariant="outline"
-      paginationLinkVariant="ghost"
-    >
-      <nav sc-pagination>
-        <ul sc-pagination-list>
-          <li>
-            <a sc-pagination-previous>
-              <svg si-chevron-left-icon></svg>
-              <span>Previous</span>
-              <span class="sr-only">Previous page</span>
-            </a>
-          </li>
-
-          @for (page of paginator.pages(); track $index) {
-            <li>
-              @if (page === '...') {
-                <span sc-pagination-ellipsis>
-                  <svg class="size-4" si-ellipsis-icon></svg>
-                  <span class="sr-only">More pages</span>
-                </span>
-              } @else {
-                <a [page]="page" sc-pagination-link>
-                  {{ page }}
-                </a>
-              }
-            </li>
-          }
-
-          <li>
-            <a sc-pagination-next>
-              <span>Next</span>
-              <svg si-chevron-right-icon></svg>
-              <span class="sr-only">Next page</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </sc-paginator>
-  \`,
-  styles: \`\`,
-  encapsulation: ViewEncapsulation.None,
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class PaginationDemo {
-  currentPage = signal<number>(1);
-  pageSize = signal<number>(10);
-  totalSize = signal<number>(65);
-
-  setPageEvent(pageEvent: ScPageEvent) {
-    this.currentPage.set(pageEvent.page);
-    this.pageSize.set(pageEvent.pageSize);
-  }
-}`;
+  protected readonly code = `import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+  
+  import { ScSeparator } from '@semantic-components/ui';
+  
+  @Component({
+    selector: 'app-separator-demo',
+    imports: [ScSeparator],
+    template: \`
+      <div class="space-y-1">
+        <h4 class="text-sm font-medium leading-none">Radix Primitives</h4>
+        <p class="text-sm text-muted-foreground">An open-source UI component library.</p>
+      </div>
+      <sc-separator class="my-4" />
+      <div class="flex h-5 items-center space-x-4 text-sm">
+        <div>Blog</div>
+        <sc-separator orientation="vertical" />
+        <div>Docs</div>
+        <sc-separator orientation="vertical" />
+        <div>Source</div>
+      </div>
+    \`,
+    styles: \`\`,
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+  })
+  export class SeparatorDemo {}`;
 }
