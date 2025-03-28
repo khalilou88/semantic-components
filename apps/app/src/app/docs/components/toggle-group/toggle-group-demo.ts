82ec37da311c1ac5ed5db4a5bbf5bb6d359a62e5
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-03-28 01:30:50 +0100

    build: work in progress

--- apps/app/src/app/docs/components/toggle-group/toggle-group-demo.ts
+++ apps/app/src/app/docs/components/toggle-group/toggle-group-demo.ts
@@ -8,9 +8,15 @@ import { SiAlignCenterIcon, SiAlignLeftIcon, SiAlignRightIcon } from '@semantic-
   imports: [ScToggleGroup, ScToggleItem, SiAlignLeftIcon, SiAlignCenterIcon, SiAlignRightIcon],
   template: `
     <sc-toggle-group>
-      <button sc-toggle-item><svg si-align-left-icon></svg></button>
-      <button sc-toggle-item><svg si-align-center-icon></svg></button>
-      <button sc-toggle-item><svg si-align-right-icon></svg></button>
+      <button value="left" aria-label="Left aligned" sc-toggle-item>
+        <svg si-align-left-icon></svg>
+      </button>
+      <button value="center" aria-label="Center aligned" sc-toggle-item>
+        <svg si-align-center-icon></svg>
+      </button>
+      <button value="right" aria-label="Right aligned" sc-toggle-item>
+        <svg si-align-right-icon></svg>
+      </button>
     </sc-toggle-group>
   `,
   styles: ``,
