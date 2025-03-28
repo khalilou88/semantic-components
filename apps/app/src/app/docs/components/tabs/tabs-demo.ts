921f5acc57542cdc0c5abdf4d808bb987ef5b189
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-03-28 14:34:47 +0100

    build: work in progress

--- apps/app/src/app/docs/components/tabs/tabs-demo.ts
+++ apps/app/src/app/docs/components/tabs/tabs-demo.ts
@@ -1,10 +1,95 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
+import {
+  ScButton,
+  ScCard,
+  ScCardContent,
+  ScCardDescription,
+  ScCardFooter,
+  ScCardHeader,
+  ScCardTitle,
+  ScInput,
+  ScLabel,
+  ScTab,
+  ScTabContent,
+  ScTabLabel,
+  ScTabs,
+} from '@semantic-components/ui';
+
 @Component({
   selector: 'app-tabs-demo',
-  imports: [],
+  imports: [
+    ScTabs,
+    ScTab,
+    ScTabLabel,
+    ScTabContent,
+    ScCard,
+    ScCardHeader,
+    ScCardTitle,
+    ScCardDescription,
+    ScCardContent,
+    ScCardFooter,
+    ScLabel,
+    ScInput,
+    ScButton,
+  ],
   template: `
-    <p>tabs-demo works!</p>
+    <sc-tabs class="w-[400px]" tabsHeaderClass="grid w-full grid-cols-2">
+      <sc-tab>
+        <sc-tab-label>Account</sc-tab-label>
+        <sc-tab-content>
+          <div sc-card>
+            <div sc-card-header>
+              <h2 sc-card-title>Account</h2>
+              <p sc-card-description>
+                Make changes to your account here. Click save when you're done.
+              </p>
+            </div>
+
+            <div class="space-y-2" sc-card-content>
+              <div class="space-y-1">
+                <label sc-label for="name">Name</label>
+                <input id="name" sc-input value="Pedro Duarte" />
+              </div>
+              <div class="space-y-1">
+                <label sc-label for="username">Username</label>
+                <input id="username" sc-input value="@peduarte" />
+              </div>
+            </div>
+            <div sc-card-footer>
+              <button sc-button type="submit">Save changes</button>
+            </div>
+          </div>
+        </sc-tab-content>
+      </sc-tab>
+
+      <sc-tab>
+        <sc-tab-label>Password</sc-tab-label>
+        <sc-tab-content>
+          <div sc-card>
+            <div sc-card-header>
+              <h2 sc-card-title>Password</h2>
+              <p sc-card-description>
+                Change your password here. After saving, you'll be logged out.
+              </p>
+            </div>
+            <div class="space-y-2" sc-card-content>
+              <div class="space-y-1">
+                <label sc-label for="current">Current password</label>
+                <input id="current" sc-input type="password" />
+              </div>
+              <div class="space-y-1">
+                <label sc-label for="new">New password</label>
+                <input id="new" sc-input type="password" />
+              </div>
+            </div>
+            <div sc-card-footer>
+              <button sc-button>Save password</button>
+            </div>
+          </div>
+        </sc-tab-content>
+      </sc-tab>
+    </sc-tabs>
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
