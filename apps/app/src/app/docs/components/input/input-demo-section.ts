e899c049b4e9b9310f0b430dc8c74fb3cbf067dd
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-03-29 03:16:01 +0100

    build: work in progress

--- apps/app/src/app/docs/components/input/input-demo-section.ts
+++ apps/app/src/app/docs/components/input/input-demo-section.ts
@@ -1,15 +1,24 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation, input } from '@angular/core';
 
+import { PreviewCodeTabs } from '../../../components/preview-code-tabs/preview-code-tabs';
 import { InputDemo } from './input-demo';
 
 @Component({
   selector: 'app-input-demo-section',
-  imports: [InputDemo],
+  imports: [PreviewCodeTabs, InputDemo],
   template: `
-    <app-input-demo />
+    <app-preview-code-tabs [code]="code" [title]="title()" [level]="level()">
+      <app-input-demo />
+    </app-preview-code-tabs>
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class InputDemoSection {}
+export class InputDemoSection {
+  readonly title = input<string>('');
+
+  readonly level = input<'2' | '3'>('2');
+
+  protected readonly code = ``;
+}
