0fd9064718ceec4db363daefc0be53716d8aa7ed
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-03-29 17:04:01 +0100

    build: work in progress

--- libs/ui/src/lib/components/radio-group/radio.ts
+++ libs/ui/src/lib/components/radio-group/radio.ts
@@ -42,10 +42,13 @@ export class ScRadio {
   protected readonly class = computed(() =>
     cn(
       'relative',
-      'appearance-none size-5 rounded-full border-2 border-gray-300 cursor-pointer checked:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-300',
+      'appearance-none',
+      // 'size-5 rounded-full border-2 border-gray-300 cursor-pointer checked:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-300',
+
+      'aspect-square h-4 w-4 rounded-full border border-primary text-primary shadow focus:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
 
       "[&::before]:content-['']",
-      '[&::before]:absolute [&::before]:top-1/2 [&::before]:left-1/2 [&::before]:-translate-x-1/2 [&::before]:-translate-y-1/2 [&::before]:size-3 [&::before]:rounded-full [&::before]:bg-blue-500 [&::before]:opacity-0 [&::before]:transform [&::before]:scale-0 [&::before]:transition-all [&::before]:duration-200',
+      '[&::before]:absolute [&::before]:top-1/2 [&::before]:left-1/2 [&::before]:-translate-x-1/2 [&::before]:-translate-y-1/2 [&::before]:size-2.5 [&::before]:rounded-full [&::before]:bg-primary [&::before]:opacity-0 [&::before]:transform [&::before]:scale-0 [&::before]:transition-all [&::before]:duration-200',
 
       '[&:checked]:[&::before]:opacity-100 [&:checked]:[&::before]:scale-100',
 
