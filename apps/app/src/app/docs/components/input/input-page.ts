533ba5cd222318925da46b4645b023e0dbe3eb33
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-03-29 03:32:00 +0100

    build: work in progress

--- apps/app/src/app/docs/components/input/input-page.ts
+++ apps/app/src/app/docs/components/input/input-page.ts
@@ -1,15 +1,28 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
 import { InputDemoSection } from './input-demo-section';
+import { InputDisabledSection } from './input-disabled-section';
+import { InputFileSection } from './input-file-section';
 import { InputFormSection } from './input-form-section';
 import { InputWithButtonSection } from './input-with-button-section';
 import { InputWithLabelSection } from './input-with-label-section';
 
 @Component({
   selector: 'app-input-page',
-  imports: [InputDemoSection, InputWithButtonSection, InputFormSection, InputWithLabelSection],
+  imports: [
+    InputDemoSection,
+    InputWithButtonSection,
+    InputFormSection,
+    InputWithLabelSection,
+    InputDisabledSection,
+    InputFileSection,
+  ],
   template: `
     <app-input-demo-section />
+
+    <app-input-file-section title="File" />
+
+    <app-input-disabled-section title="Disabled" />
 
     <app-input-with-label-section title="With Label" />
 
