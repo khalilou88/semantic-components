efecc12caaba5dbe66056244376fc46b8234c34f
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-03-29 03:37:57 +0100

    build: work in progress

--- apps/app/src/app/docs/components/input/input-file.ts
+++ apps/app/src/app/docs/components/input/input-file.ts
@@ -1,10 +1,15 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
+import { ScInput, ScLabel } from '@semantic-components/ui';
+
 @Component({
   selector: 'app-input-file',
-  imports: [],
+  imports: [ScInput, ScLabel],
   template: `
-    <p>input-file works!</p>
+    <div class="grid w-full max-w-sm items-center gap-1.5">
+      <label sc-label for="picture">Picture</label>
+      <input id="picture" sc-input type="file" />
+    </div>
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
