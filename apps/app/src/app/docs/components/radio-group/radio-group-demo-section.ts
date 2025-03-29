f2f37d9553ebb1cc7804b5ad755410654c07fe04
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-03-29 03:56:37 +0100

    build: work in progress

--- apps/app/src/app/docs/components/radio-group/radio-group-demo-section.ts
+++ apps/app/src/app/docs/components/radio-group/radio-group-demo-section.ts
@@ -1,15 +1,24 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation, input } from '@angular/core';
 
+import { PreviewCodeTabs } from '../../../components/preview-code-tabs/preview-code-tabs';
 import { RadioGroupDemo } from './radio-group-demo';
 
 @Component({
   selector: 'app-radio-group-demo-section',
-  imports: [RadioGroupDemo],
+  imports: [PreviewCodeTabs, RadioGroupDemo],
   template: `
-    <app-radio-group-demo />
+    <app-preview-code-tabs [code]="code" [title]="title()" [level]="level()">
+      <app-radio-group-demo />
+    </app-preview-code-tabs>
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RadioGroupDemoSection {}
+export class RadioGroupDemoSection {
+  readonly title = input<string>('');
+
+  readonly level = input<'2' | '3'>('2');
+
+  protected readonly code = ``;
+}
--- apps/app/src/app/docs/components/radio-group/radio-group-form-section.ts
+++ apps/app/src/app/docs/components/radio-group/radio-group-form-section.ts
@@ -1,15 +1,24 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation, input } from '@angular/core';
 
+import { PreviewCodeTabs } from '../../../components/preview-code-tabs/preview-code-tabs';
 import { RadioGroupForm } from './radio-group-form';
 
 @Component({
   selector: 'app-radio-group-form-section',
-  imports: [RadioGroupForm],
+  imports: [PreviewCodeTabs, RadioGroupForm],
   template: `
-    <app-radio-group-form />
+    <app-preview-code-tabs [code]="code" [title]="title()" [level]="level()">
+      <app-radio-group-form />
+    </app-preview-code-tabs>
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RadioGroupFormSection {}
+export class RadioGroupFormSection {
+  readonly title = input<string>('');
+
+  readonly level = input<'2' | '3'>('2');
+
+  protected readonly code = ``;
+}
