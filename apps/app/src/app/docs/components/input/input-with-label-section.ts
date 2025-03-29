cfd39d51257c23c41a4a2d38bab24b6553b0f527
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-03-29 03:30:10 +0100

    build: work in progress

--- apps/app/src/app/docs/components/input/input-with-label-section.ts
+++ apps/app/src/app/docs/components/input/input-with-label-section.ts
@@ -1,15 +1,24 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation, input } from '@angular/core';
 
+import { PreviewCodeTabs } from '../../../components/preview-code-tabs/preview-code-tabs';
 import { InputWithLabel } from './input-with-label';
 
 @Component({
   selector: 'app-input-with-label-section',
-  imports: [InputWithLabel],
+  imports: [PreviewCodeTabs, InputWithLabel],
   template: `
-    <app-input-with-label />
+    <app-preview-code-tabs [code]="code" [title]="title()" [level]="level()">
+      <app-input-with-label />
+    </app-preview-code-tabs>
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class InputWithLabelSection {}
+export class InputWithLabelSection {
+  readonly title = input<string>('');
+
+  readonly level = input<'2' | '3'>('2');
+
+  protected readonly code = ``;
+}
