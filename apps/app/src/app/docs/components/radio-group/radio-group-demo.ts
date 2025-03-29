c41c479c0d6fc9ea8a1511e4e87919369cc96170
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-03-29 15:08:13 +0100

    build: work in progress

--- apps/app/src/app/docs/components/radio-group/radio-group-demo.ts
+++ apps/app/src/app/docs/components/radio-group/radio-group-demo.ts
@@ -1,13 +1,37 @@
+import { JsonPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+import { ScRadioGroup, ScRadioItem } from '@semantic-components/ui';
 
 @Component({
   selector: 'app-radio-group-demo',
-  imports: [],
+  imports: [ScRadioGroup, ScRadioItem, ReactiveFormsModule, JsonPipe],
   template: `
-    <p>radio-group-demo works!</p>
+    <form [formGroup]="radioGroupForm">
+      <sc-radio-group value="comfortable" formControlName="radio">
+        <sc-radio-item id="r1" value="default">Default radio</sc-radio-item>
+
+        <sc-radio-item id="r2" value="comfortable">Comfortable</sc-radio-item>
+
+        <sc-radio-item id="r3" value="compact">Compact</sc-radio-item>
+      </sc-radio-group>
+    </form>
+
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+
+    {{ radioGroupForm.value | json }}
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RadioGroupDemo {}
+export class RadioGroupDemo {
+  radioGroupForm = new FormGroup({
+    radio: new FormControl('compact'),
+  });
+}
