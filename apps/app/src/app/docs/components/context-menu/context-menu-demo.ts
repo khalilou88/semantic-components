6e241686fb00631c99b8e589902e8332e9d5e9bb
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-03-29 04:17:12 +0100

    build: work in progress

--- apps/app/src/app/docs/components/context-menu/context-menu-demo.ts
+++ apps/app/src/app/docs/components/context-menu/context-menu-demo.ts
@@ -1,10 +1,25 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
+import { ScContextMenuTriggerFor, ScMenu, ScMenuItem } from '@semantic-components/ui';
+
 @Component({
   selector: 'app-context-menu-demo',
-  imports: [],
+  imports: [ScContextMenuTriggerFor, ScMenu, ScMenuItem],
   template: `
-    <p>context-menu-demo works!</p>
+    <div
+      class="flex h-[150px] w-[300px] items-center justify-center rounded-md border border-dashed text-sm"
+      [scContextMenuTriggerFor]="context_menu"
+    >
+      Right click here
+    </div>
+
+    <ng-template #context_menu>
+      <div sc-menu>
+        <button sc-menu-item>Cut</button>
+        <button sc-menu-item>Copy</button>
+        <button sc-menu-item>Link</button>
+      </div>
+    </ng-template>
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
