023349941ea854cfbf19ad8fa2a2299a673666fa
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-04-04 23:47:57 +0200

    build: work in progress

--- apps/app/src/app/docs/getting-started/installation/installation-page.ts
+++ apps/app/src/app/docs/getting-started/installation/installation-page.ts
@@ -4,7 +4,21 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   selector: 'app-installation-page',
   imports: [],
   template: `
-    <p>page works!</p>
+    <div class="bg-primary">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        style="stroke: var(--primary-foreground)"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path d="M20 6 9 17l-5-5" />
+      </svg>
+    </div>
   `,
   styles: ``,
   encapsulation: ViewEncapsulation.None,
