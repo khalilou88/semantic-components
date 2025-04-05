ab0b138b201c61f3b867d85e4049eefb89c7df30
khalilou88 <khalil.lagrida@gmail.com>
2025-04-05 18:57:28 +0200

    build: work in progress

--- apps/app/eslint.config.cjs
+++ apps/app/eslint.config.cjs
@@ -8,6 +8,7 @@ module.exports = [
   {
     files: ['**/*.ts'],
     rules: {
+      '@angular-eslint/no-input-rename': ['warn'],
       '@angular-eslint/component-class-suffix': 'off',
       '@angular-eslint/directive-selector': [
         'warn',
