f56caa785a39d5f22856396aeb2cc729afa676ed
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-05-31 12:32:19 +0200

    build: work in progress

--- apps/ui-test-pages/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from '@angular/router';
-
-export const appRoutes: Route[] = [
-  {
-    path: 'input-otp',
-    loadComponent: () => import('./input-otp/page'),
-    title: 'Input OTP Page',
-  },
-];
