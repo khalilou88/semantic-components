25affee487e428120f895e8ba0e5a7cf9bb9c381
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-06-13 08:02:27 +0200

    build: work in progress

--- apps/image-cropper-showcase/src/app/app.config.ts
+++ apps/image-cropper-showcase/src/app/app.config.ts
@@ -1,8 +1,8 @@
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideExperimentalZonelessChangeDetection(), provideRouter(appRoutes)],
+  providers: [provideZonelessChangeDetection(), provideRouter(appRoutes)],
 };
